db901e86354d4bcb130c3b4e4ea880be590209cd
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-12-30 21:17:09 -0600

    Complete Day 3

--- AdventOfCode2015/Day03/Day03.cs
+++ AdventOfCode2015/Day03/Day03.cs
@@ -28,7 +28,7 @@ namespace AdventOfCode2015
             int yPos = 0;
             List<string> houses = new List<string>
             {
-                xPos.ToString() + yPos.ToString()
+                HouseLabel(xPos, yPos)
             };
 
             foreach (char c in input1)
@@ -51,7 +51,7 @@ namespace AdventOfCode2015
                         Print.PrintErrorAndExit("Unaccepted character in input file " + input1Path);
                         break;
                 }
-                string houseLabel = xPos.ToString() + yPos.ToString();
+                string houseLabel = HouseLabel(xPos, yPos);
                 if (!houses.Contains(houseLabel))
                 {
                     houses.Add(houseLabel);
@@ -59,6 +59,82 @@ namespace AdventOfCode2015
             }
 
             Console.WriteLine("Day 3 Part One Answer: " + houses.Count);
+
+
+            int santaXPos = 0;
+            int santaYPos = 0;
+            int robotXPos = 0;
+            int robotYPos = 0;
+            houses = new List<string>
+            {
+                HouseLabel(santaXPos, santaYPos)
+            };
+
+            bool santaTurn = true;
+            foreach (char c in input1)
+            {
+                if (santaTurn)
+                {
+                    switch (c)
+                    {
+                        case '^':
+                            santaYPos++;
+                            break;
+                        case 'v':
+                            santaYPos--;
+                            break;
+                        case '>':
+                            santaXPos++;
+                            break;
+                        case '<':
+                            santaXPos--;
+                            break;
+                        default:
+                            Print.PrintErrorAndExit("Unaccepted character in input file " + input1Path);
+                            break;
+                    }
+                    string houseLabel = HouseLabel(santaXPos, santaYPos);
+                    if (!houses.Contains(houseLabel))
+                    {
+                        houses.Add(houseLabel);
+                    }
+                    santaTurn = false;
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case '^':
+                            robotYPos++;
+                            break;
+                        case 'v':
+                            robotYPos--;
+                            break;
+                        case '>':
+                            robotXPos++;
+                            break;
+                        case '<':
+                            robotXPos--;
+                            break;
+                        default:
+                            Print.PrintErrorAndExit("Unaccepted character in input file " + input1Path);
+                            break;
+                    }
+                    string houseLabel = HouseLabel(robotXPos, robotYPos);
+                    if (!houses.Contains(houseLabel))
+                    {
+                        houses.Add(houseLabel);
+                    }
+                    santaTurn = true;
+                }
+            }
+
+            Console.WriteLine("Day 3 Part Two Answer: " + houses.Count);
+        }
+
+        static string HouseLabel(int x, int y)
+        {
+            return x + "," + y;
         }
     }
 }
