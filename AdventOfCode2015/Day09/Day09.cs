fbf242f2149147a57ef01116798642c3ee91bed7
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-01-16 19:57:31 -0600

    Create table of input data

--- AdventOfCode2015/Day09/Day09.cs
+++ AdventOfCode2015/Day09/Day09.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 
 namespace AdventOfCode2015
@@ -26,6 +27,10 @@ namespace AdventOfCode2015
 
             List<string> locations = new List<string>();
 
+            DataTable distanceTable = new DataTable("Distances");
+            distanceTable.Columns.Add(new DataColumn() { DataType = typeof(string), ColumnName = "Location" });
+            distanceTable.PrimaryKey = new DataColumn[1] { distanceTable.Columns["Location"] };
+
             foreach (string line in input1)
             {
                 string[] data = line.Split();
@@ -40,22 +45,63 @@ namespace AdventOfCode2015
                     continue;
                 }
 
-                if (!locations.Contains(data[0])) locations.Add(data[0]);
-                if (!locations.Contains(data[2])) locations.Add(data[2]);
+                if (!locations.Contains(data[0]))
+                {
+                    AddRowAndColumn(data[0], distanceTable);
+                    locations.Add(data[0]);
+                }
+                if (!locations.Contains(data[2]))
+                {
+                    AddRowAndColumn(data[2], distanceTable);
+                    locations.Add(data[2]);
+                }
 
-
-
+                SetValue(data[0], data[2], distance, distanceTable);
             }
-
-            Console.WriteLine("{0} locations found:", locations.Count);
             foreach (string location in locations)
             {
-                Console.WriteLine(location);
+                SetValue(location, location, 0, distanceTable);
             }
+
+            int numOfPermutations = Factorial(locations.Count);
 
 
 
             Console.WriteLine("Day 9 Part One Answer: " + "TO BE IMPLEMENTED");
+        }
+
+        static void AddRowAndColumn(string name, DataTable table)
+        {
+            table.Columns.Add(new DataColumn() { DataType = typeof(int), ColumnName = name });
+            DataRow row = table.NewRow();
+            row["Location"] = name;
+            table.Rows.Add(row);
+        }
+
+        static void SetValue(string name1, string name2, int value, DataTable table)
+        {
+            DataRow row = table.Rows.Find(name1);
+            row[name2] = value;
+            row = table.Rows.Find(name2);
+            row[name1] = value;
+        }
+
+        static int GetValue(string name1, string name2, DataTable table)
+        {
+            DataRow row = table.Rows.Find(name1);
+            return (int)row[name2];
+        }
+
+        static int Factorial(int n)
+        {
+            if (n < 0) throw new ArgumentOutOfRangeException();
+            int output = 1;
+            while (n > 1)
+            {
+                output *= n;
+                n--;
+            }
+            return output;
         }
 
         static void PrintErrorInInput(string text)
