ba112e14b925c29dd501a2fe2f86a6fcb9792f65
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-01-22 22:25:31 -0600

    Create table of input data (Day 16)

--- AdventOfCode2015/Day16/Day16.cs
+++ AdventOfCode2015/Day16/Day16.cs
@@ -5,7 +5,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2015
 {
@@ -23,6 +25,42 @@ namespace AdventOfCode2015
             {
                 Print.PrintErrorAndExit("Unable to load input file " + input1Path + Environment.NewLine + e.GetType());
             }
+
+            DataTable sueTable = new DataTable();
+            sueTable.Columns.Add(new DataColumn() { DataType = typeof(string), ColumnName = "id", Unique = true });
+            sueTable.PrimaryKey = new DataColumn[1] { sueTable.Columns["id"] };
+            string[] columnNames = new string[] { "children", "cats", "samoyeds", "pomeranians", "akitas", "vizslas", "goldfish", "trees", "cars", "perfumes" };
+            foreach (string name in columnNames)
+            {
+                sueTable.Columns.Add(new DataColumn() { DataType = typeof(int), ColumnName = name });
+            }
+
+            foreach (string line in input1)
+            {
+                string[] words = line.Split();
+                if (words.Length < 4 || words[0] != "Sue") continue;
+                string key = words[1].Trim(':');
+                if (sueTable.Rows.Contains(key))
+                {
+                    Print.PrintErrorAndExit("Day 16: Duplicate ID \"" + key + "\" in Input1.txt");
+                }
+                DataRow row = sueTable.NewRow();
+                row["id"] = key;
+                for (int i = 2; i < words.Length - 1; i += 2)
+                {
+                    if (int.TryParse(words[i + 1].Trim(','), out int value))
+                    {
+                        string column = words[i].Trim(':');
+                        if (!columnNames.Contains(column)) Print.PrintErrorAndExit("Day 16: Invalid data \"" + words[i] + " " + words[i + 1] + "\" in Input1.txt");
+                        row[column] = value;
+                    }
+                    else Print.PrintErrorAndExit("Day 16: Invalid data \"" + words[i] + " " + words[i + 1] + "\" in Input1.txt");
+                }
+                sueTable.Rows.Add(row);
+            }
+
+            Console.WriteLine("Number of rows in input = {0}", sueTable.Rows.Count);
+
 
             List<string> potentialMatches = new List<string>();
 
@@ -86,6 +124,9 @@ namespace AdventOfCode2015
                 Console.WriteLine("Day 16 Part One: Compare the following potential matches manually.");
                 foreach (string sue in potentialMatches) Console.WriteLine(" >> " + sue);
             }
+
+
+            Console.WriteLine("Day 16 Part One Answer: " + "To Be Implemented Properly");
         }
     }
 }
