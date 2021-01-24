cab4eeb6efee2b13b6eb72212490b962fff68bb1
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-01-24 13:53:34 -0600

    Complete Day 18 Part One

--- AdventOfCode2015/Program.cs
+++ AdventOfCode2015/Program.cs
@@ -24,7 +24,8 @@ namespace AdventOfCode2015
             //Day14.Solve();
             //Day15.Solve();
             //Day16.Solve();
-            Day17.Solve();
+            //Day17.Solve();
+            Day18.Solve();
 
             Console.ReadKey(true);
         }
