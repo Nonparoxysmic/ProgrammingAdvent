1d8b90c62d9ad87a5935a94dddbfe7ec0551293e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-28 22:48:31 -0500

    Complete Day 2 Part One

--- ProgrammingAdvent2016/Solutions/Day02.cs
+++ ProgrammingAdvent2016/Solutions/Day02.cs
@@ -11,7 +11,7 @@ namespace ProgrammingAdvent2016
             string[] input;
             try
             {
-                input = File.ReadAllLines(@"InputFiles\InputDay01Part1.txt");
+                input = File.ReadAllLines(@"InputFiles\InputDay02Part1.txt");
             }
             catch
             {
@@ -19,8 +19,37 @@ namespace ProgrammingAdvent2016
                 return;
             }
 
-            partOneTextBox.Text = "Day 2 Solution Not Yet Implemented";
-            partTwoTextBox.Text = "Day 2 Solution Not Yet Implemented";
+            // 1 2 3
+            // 4 5 6
+            // 7 8 9
+            int row = 1;
+            int col = 1;
+            string partOneAnswer = "";
+            foreach (string line in input)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (row > 0) row--;
+                            break;
+                        case 'D':
+                            if (row < 2) row++;
+                            break;
+                        case 'L':
+                            if (col > 0) col--;
+                            break;
+                        case 'R':
+                            if (col < 2) col++;
+                            break;
+                    }
+                }
+                partOneAnswer += (row * 3 + col + 1);
+            }
+            partOneTextBox.Text = partOneAnswer;
+
+            partTwoTextBox.Text = "Day 2 Part Two Solution Not Yet Implemented";
         }
     }
 }
