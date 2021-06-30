355d0081301e29748397aa1b1fd4769ab99c7ad1
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-29 23:46:34 -0500

    Complete Day 2

--- ProgrammingAdvent2016/Solutions/Day02.cs
+++ ProgrammingAdvent2016/Solutions/Day02.cs
@@ -49,7 +49,42 @@ namespace ProgrammingAdvent2016
             }
             partOneTextBox.Text = partOneAnswer;
 
-            partTwoTextBox.Text = "Day 2 Part Two Solution Not Yet Implemented";
+            //     1
+            //   2 3 4
+            // 5 6 7 8 9
+            //   A B C
+            //     D
+            string[,] grid = new string[,] { {  "",  "", "5",  "",  "" },
+                                             {  "", "2", "6", "A",  "" },
+                                             { "1", "3", "7", "B", "D" },
+                                             {  "", "4", "8", "C",  "" },
+                                             {  "",  "", "9",  "",  "" } };
+            col = 0;
+            row = 2;
+            string partTwoAnswer = "";
+            foreach (string line in input)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (row > 0 && grid[col, row - 1] != "") row--;
+                            break;
+                        case 'D':
+                            if (row < 4 && grid[col, row + 1] != "") row++;
+                            break;
+                        case 'L':
+                            if (col > 0 && grid[col - 1, row] != "") col--;
+                            break;
+                        case 'R':
+                            if (col < 4 && grid[col + 1, row] != "") col++;
+                            break;
+                    }
+                }
+                partTwoAnswer += grid[col, row];
+            }
+            partTwoTextBox.Text = partTwoAnswer;
         }
     }
 }
