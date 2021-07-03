6ee965ff0a27789d359b85e87646f42edd526b0b
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-07-02 19:32:59 -0500

    Complete Day 3 Part One

--- ProgrammingAdvent2016/Solutions/Day03.cs
+++ ProgrammingAdvent2016/Solutions/Day03.cs
@@ -19,20 +19,26 @@ namespace ProgrammingAdvent2016
                 return;
             }
 
+            int possibleTriangles = 0;
             foreach (string line in input)
             {
                 string[] inputs = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
                 if (inputs.Length != 3) continue;
-                int[] values = new int[3];
+                int sum = 0;
+                int max = 0;
                 for (int i = 0; i < 3; i++)
                 {
-                    if (!int.TryParse(inputs[i], out values[i]))
+                    if (!int.TryParse(inputs[i], out int value))
                     {
-                        
+                        partOneTextBox.Text = "ERROR: Invalid data \"" + inputs[i] + "\" in input.";
+                        return;
                     }
+                    sum += value;
+                    max = Math.Max(max, value);
                 }
+                if (sum - max > max) possibleTriangles++;
             }
-            partOneTextBox.Text = "Part One Solution Not Yet Implemented";
+            partOneTextBox.Text = possibleTriangles.ToString();
 
             partTwoTextBox.Text = "Part Two Solution Not Yet Implemented";
         }
