a06c7d026f3d1e5303f5d8ece5d0ddf5c7b3b183
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-07-03 20:03:21 -0500

    Complete Day 3

--- ProgrammingAdvent2016/Solutions/Day03.cs
+++ ProgrammingAdvent2016/Solutions/Day03.cs
@@ -39,8 +39,35 @@ namespace ProgrammingAdvent2016
                 if (sum - max > max) possibleTriangles++;
             }
             partOneTextBox.Text = possibleTriangles.ToString();
-
-            partTwoTextBox.Text = "Part Two Solution Not Yet Implemented";
+            
+            if (input.Length < 3 || input.Length % 3 != 0)
+            {
+                partTwoTextBox.Text = "ERROR: Invalid input.";
+                return;
+            }
+            possibleTriangles = 0;
+            for (int i = 0; i < input.Length; i += 3)
+            {
+                string[][] dataArrays = new string[3][];
+                dataArrays[0] = input[i].Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                dataArrays[1] = input[i + 1].Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                dataArrays[2] = input[i + 2].Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                if (dataArrays[0].Length != 3 || dataArrays[1].Length != 3 || dataArrays[2].Length != 3) continue;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (!int.TryParse(dataArrays[0][j], out int value0) ||
+                        !int.TryParse(dataArrays[1][j], out int value1) ||
+                        !int.TryParse(dataArrays[2][j], out int value2))
+                    {
+                        partTwoTextBox.Text = "ERROR: Invalid data in input.";
+                        return;
+                    }
+                    int sum = value0 + value1 + value2;
+                    int max = Math.Max(value0, Math.Max(value1, value2));
+                    if (sum - max > max) possibleTriangles++;
+                }
+            }
+            partTwoTextBox.Text = possibleTriangles.ToString();
         }
     }
 }
