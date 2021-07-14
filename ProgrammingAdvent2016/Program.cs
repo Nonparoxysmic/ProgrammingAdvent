cc40be6f98df790dbfb69bc68be09df49bb6409c
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-07-13 23:55:39 -0500

    Add method to read input files

--- ProgrammingAdvent2016/Program.cs
+++ ProgrammingAdvent2016/Program.cs
@@ -13,6 +13,20 @@ namespace ProgrammingAdvent2016
             Application.Run(new Form1());
         }
 
+        public static bool ReadInputFile(out string text, string path)
+        {
+            try
+            {
+                text = System.IO.File.ReadAllText(path);
+                return true;
+            }
+            catch
+            {
+                text = "ERROR";
+                return false;
+            }
+        }
+
         public static string[] ToLines(this string input)
         {
             return input.TrimEnd(new[] { '\r', '\n' }).Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
