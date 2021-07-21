f50f4c6e01fd7da69dfe3981d2021b4cb09a02d3
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-07-20 23:49:39 -0500

    Complete Day 5 Part One

--- ProgrammingAdvent2016/Solutions/Day05.cs
+++ ProgrammingAdvent2016/Solutions/Day05.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Text;
 
 namespace ProgrammingAdvent2016
 {
@@ -23,7 +24,30 @@ namespace ProgrammingAdvent2016
             input = input.Trim();
             stopwatch.Start();
 
+            StringBuilder sb = new StringBuilder();
+            byte[] hashFunctionInput = Encoding.UTF8.GetBytes(input + "0");
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                byte[] hash = AdventMD5.ComputeHash(hashFunctionInput);
+                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 0x10)
+                {
+                    // Five leading zeroes found.
+                    sb.Append(hash[2].ToString("x"));
+                    if (sb.Length >= 8) break;
+                }
 
+                // Recalculate the hash function input if the last byte reaches '9' (57):
+                    if (hashFunctionInput[hashFunctionInput.GetUpperBound(0)] > 56)
+                {
+                    hashFunctionInput = Encoding.UTF8.GetBytes(input + i);
+                }
+                // Increment the counter on the hash function input by incrementing the last byte:
+                else
+                {
+                    hashFunctionInput[hashFunctionInput.GetUpperBound(0)]++;
+                }
+            }
+            solution.WriteSolution(1, sb.ToString(), stopwatch.ElapsedMilliseconds);
 
             stopwatch.Reset();
             return solution;
