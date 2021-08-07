f52f0c4f030a91bd610ef05cb89cbaeb68ff9de1
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-08-06 22:18:00 -0500

    Complete Day 9 Part One

--- ProgrammingAdvent2016/Solutions/Day09.cs
+++ ProgrammingAdvent2016/Solutions/Day09.cs
@@ -34,7 +34,38 @@ namespace ProgrammingAdvent2016
             {
                 if (input[i] == '(')
                 {
-                    // TODO: process marker
+                    // Start of a marker found.
+                    int endPos = -1;
+                    for (int j = i; j < input.Length; j++)
+                    {
+                        if (input[j] == ')')
+                        {
+                            // End of marker found.
+                            endPos = j;
+                            break;
+                        }
+                    }
+                    if (endPos == -1 || endPos - i < 4)
+                    {
+                        // Not a valid marker.
+                        decompressed.Clear();
+                        decompressed.Append("ERROR");
+                        break;
+                    }
+                    string[] markerValues = input.Substring(i + 1, endPos - i - 1).Split('x');
+                    if (markerValues.Length != 2 || !int.TryParse(markerValues[0], out int charNum) || !int.TryParse(markerValues[1], out int repeats))
+                    {
+                        // Not a valid marker.
+                        decompressed.Clear();
+                        decompressed.Append("ERROR");
+                        break;
+                    }
+                    string data = input.Substring(endPos + 1, charNum);
+                    for (int k = 0; k < repeats; k++)
+                    {
+                        decompressed.Append(data);
+                    }
+                    i = endPos + charNum;
                 }
                 else decompressed.Append(input[i]);
             }
