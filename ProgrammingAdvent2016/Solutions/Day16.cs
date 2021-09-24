67ae3e4cd3092f8ec78f17c5467484cd5640a9c1
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-09-23 20:24:37 -0500

    Complete Day 16

--- ProgrammingAdvent2016/Solutions/Day16.cs
+++ ProgrammingAdvent2016/Solutions/Day16.cs
@@ -3,7 +3,7 @@
 // Puzzle solution by Nonparoxysmic
 // https://github.com/Nonparoxysmic/ProgrammingAdvent
 
-using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -36,29 +36,99 @@ namespace ProgrammingAdvent2016
                 return solution;
             }
 
-            
+            string partOneSolution = GenerateChecksum(input, 272);
+            solution.WriteSolution(1, partOneSolution, stopwatch.ElapsedMilliseconds);
+
+            string partTwoSolution = GenerateChecksum(input, 35651584);
+            solution.WriteSolution(2, partTwoSolution, stopwatch.ElapsedMilliseconds);
 
             stopwatch.Reset();
             return solution;
         }
 
-        string ReverseAndInvertBits(string input)
+        string GenerateChecksum(string generatorInput, int sequenceLength)
         {
-            if (input.Length == 0) return input;
-            StringBuilder sb = new StringBuilder();
-            for (int i = input.Length - 1; i >= 0; i--)
+            var sequenceGenerator = new SequenceGenerator(generatorInput);
+
+            int checksumLength = sequenceLength;
+            int iterations = 0;
+            while (checksumLength % 2 == 0)
             {
-                switch (input[i])
+                checksumLength /= 2;
+                iterations++;
+            }
+
+            Queue<char>[] queues = new Queue<char>[iterations + 1];
+            for (int i = 0; i < queues.Length; i++)
+            {
+                queues[i] = new Queue<char>();
+            }
+
+            while (true)
+            {
+                queues[0].Enqueue(sequenceGenerator.Next());
+                queues[0].Enqueue(sequenceGenerator.Next());
+                for (int i = 1; i < queues.Length; i++)
                 {
-                    case '0':
-                        sb.Append('1');
-                        break;
-                    case '1':
-                        sb.Append('0');
-                        break;
+                    if (queues[i - 1].Count > 1)
+                    {
+                        char a = queues[i - 1].Dequeue();
+                        char b = queues[i - 1].Dequeue();
+                        if (a == b) { queues[i].Enqueue('1'); }
+                        else { queues[i].Enqueue('0'); }
+                    }
                 }
+                if (queues[queues.Length - 1].Count >= checksumLength)
+                {
+                    break;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (queues[queues.Length - 1].Count > 0)
+            {
+                sb.Append(queues[queues.Length - 1].Dequeue());
             }
             return sb.ToString();
         }
+    }
+}
+
+class SequenceGenerator
+{
+    readonly static string zeroSequence = "0010011000110110001001110011011"
+        + "000100110001101110010011100110110001001100011011"
+        + "000100111001101110010011000110111001001110011011";
+
+    readonly string seed;
+
+    int direction = 1;
+    int position = -1;
+    SequenceGenerator subsequenceGenerator = null;
+
+    public SequenceGenerator(string seed)
+    {
+        this.seed = seed;
+    }
+
+    public char Next()
+    {
+        position += direction;
+        if (position < 0 || position >= seed.Length)
+        {
+            direction *= -1;
+            if (subsequenceGenerator == null)
+            {
+                subsequenceGenerator = new SequenceGenerator(zeroSequence);
+            }
+            return subsequenceGenerator.Next();
+        }
+        char output = seed[position];
+        if (direction < 0)
+        {
+            if (output == '0') { return '1'; }
+            else if (output == '1') { return '0'; }
+        }
+        return output;
     }
 }
