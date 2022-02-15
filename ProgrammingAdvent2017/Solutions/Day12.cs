e9fdfee78e3b30350deb62de87a378c0a4939c6e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-02-14 18:50:23 -0600

    Complete Day 12 Part One

---
+++ ProgrammingAdvent2017/Solutions/Day12.cs
@@ -0,0 +1,79 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2017
+// https://adventofcode.com/2017
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using ProgrammingAdvent2017.Program;
+
+namespace ProgrammingAdvent2017.Solutions
+{
+    internal class Day12 : Day
+    {
+        VillageProgram[] programs;
+
+        internal override PuzzleAnswers Solve(string input)
+        {
+            PuzzleAnswers output = new PuzzleAnswers();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            if (input == "")
+            {
+                output.WriteError("No input.", sw);
+                return output;
+            }
+            string[] inputLines = input.ToLines();
+            programs = new VillageProgram[inputLines.Length];
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                if (!Regex.IsMatch(inputLines[i], @"^\d+ <-> \d+(, \d+(?=,|$))*$")
+                    || Regex.Match(inputLines[i], @"^\d+(?= )").Value != i.ToString())
+                {
+                    output.WriteError($"Invalid input \"{inputLines[i]}\".", sw);
+                    return output;
+                }
+                programs[i] = new VillageProgram(i, inputLines[i]);
+            }
+
+            int connectedToZero = CountConnectedPrograms(0, new HashSet<int>());
+
+            sw.Stop();
+            output.WriteAnswers(connectedToZero, null, sw);
+            return output;
+        }
+
+        private int CountConnectedPrograms(int id, HashSet<int> searched)
+        {
+            foreach (int n in programs[id].Neighbors)
+            {
+                if (searched.Add(n))
+                {
+                    CountConnectedPrograms(n, searched);
+                }
+            }
+            return searched.Count;
+        }
+    }
+
+    internal class VillageProgram
+    {
+        internal int ID { get; set; }
+        internal int[] Neighbors { get; set; }
+
+        public VillageProgram(int id, string inputLine)
+        {
+            ID = id;
+            string[] neighbors = Regex.Match(inputLine, @"(?<= <-> ).+$").Value
+                .Split(new char[] { ',', ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+            List<int> neighborIDs = new List<int>();
+            foreach (string neighbor in neighbors)
+            {
+                neighborIDs.Add(int.Parse(neighbor));
+            }
+            Neighbors = neighborIDs.ToArray();
+        }
+    }
+}
