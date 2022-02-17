0e67a3eeb70e2d531d1b0516a3c02459f8c83ee5
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-02-16 19:03:47 -0600

    Complete Day 13

--- ProgrammingAdvent2017/Solutions/Day13.cs
+++ ProgrammingAdvent2017/Solutions/Day13.cs
@@ -3,7 +3,6 @@
 // for Advent of Code 2017
 // https://adventofcode.com/2017
 
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
@@ -13,8 +12,6 @@ namespace ProgrammingAdvent2017.Solutions
 {
     internal class Day13 : Day
     {
-        readonly Dictionary<int, int> depthsAndRanges = new Dictionary<int, int>();
-
         internal override PuzzleAnswers Solve(string input)
         {
             PuzzleAnswers output = new PuzzleAnswers();
@@ -27,11 +24,12 @@ namespace ProgrammingAdvent2017.Solutions
                 return output;
             }
             string[] inputLines = input.ToLines();
+            Dictionary<int, int> depthsAndRanges = new Dictionary<int, int>();
             foreach (string line in inputLines)
             {
                 if (!Regex.IsMatch(line, @"^\d+: \d+$"))
                 {
-                    output.WriteError($"Invalid input \"{line}\".", sw);
+                    output.WriteError($"Invalid line in input: \"{line}\".", sw);
                     return output;
                 }
                 int depth = int.Parse(Regex.Match(line, @"^\d+(?=: )").Value);
@@ -42,20 +40,35 @@ namespace ProgrammingAdvent2017.Solutions
             int severity = 0;
             foreach (KeyValuePair<int, int> kvp in depthsAndRanges)
             {
-                if (CaughtOnLayer(kvp.Key, 0))
+                if (CaughtOnLayer(kvp.Key, kvp.Value, 0))
                 {
                     severity += kvp.Key * kvp.Value;
                 }
             }
 
+            int delay;
+            for (delay = 0; delay < int.MaxValue; delay++)
+            {
+                bool caught = false;
+                foreach (KeyValuePair<int, int> kvp in depthsAndRanges)
+                {
+                    if (CaughtOnLayer(kvp.Key, kvp.Value, delay))
+                    {
+                        caught = true;
+                        break;
+                    }
+                }
+                if (!caught) { break; }
+            }
+
             sw.Stop();
-            output.WriteAnswers(severity, null, sw);
+            output.WriteAnswers(severity, delay, sw);
             return output;
         }
 
-        private bool CaughtOnLayer(int depth, int startTime)
+        private bool CaughtOnLayer(int depth, int range, int startTime)
         {
-            int cycleLength = 2 * (depthsAndRanges[depth] - 1);
+            int cycleLength = 2 * (range - 1);
             int arrivalTime = depth + startTime;
             if (arrivalTime % cycleLength == 0) { return true; }
             return false;
