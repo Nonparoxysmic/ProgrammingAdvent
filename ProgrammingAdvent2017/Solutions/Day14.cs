5d68d76eef376eff29be65bf8dac0f094c5380da
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-02-18 19:39:27 -0600

    Complete Day 14

--- ProgrammingAdvent2017/Solutions/Day14.cs
+++ ProgrammingAdvent2017/Solutions/Day14.cs
@@ -10,9 +10,20 @@ namespace ProgrammingAdvent2017.Solutions
 {
     internal class Day14 : Day
     {
-        internal static int[] bitsSet = new int[]
+        private readonly int[] bitsSet = new int[]
         {
             0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
+        };
+
+        private readonly string[] squarePatterns = new string[]
+        {
+            "....", "...#", "..#.", "..##", ".#..", ".#.#", ".##.", ".###",
+            "#...", "#..#", "#.#.", "#.##", "##..", "##.#", "###.", "####"
+        };
+
+        private readonly (int, int)[] directions = new (int, int)[]
+        {
+            (1, 0), (-1, 0), (0, 1), (0, -1)
         };
 
         internal override PuzzleAnswers Solve(string input)
@@ -28,27 +39,59 @@ namespace ProgrammingAdvent2017.Solutions
                 return output;
             }
 
-            int sum = 0;
-            for (int i = 0; i < 128; i++)
+            int totalBitsSet = 0;
+            char[,] grid = new char[128, 128];
+            for (int row = 0; row < 128; row++)
             {
-                byte[] hash = Day10.KnotHashBytes(input + "-" + i);
-                int bits = CountBitsSet(hash);
-                sum += bits;
+                string hash = Day10.KnotHash(input + "-" + row);
+                for (int c = 0; c < hash.Length; c++)
+                {
+                    int nibble;
+                    if (hash[c] > 57) { nibble = hash[c] - 87; }
+                    else { nibble = hash[c] - 48; }
+                    totalBitsSet += bitsSet[nibble];
+                    for (int bit = 0; bit < 4; bit++)
+                    {
+                        int col = 4 * c + bit;
+                        grid[col, row] = squarePatterns[nibble][bit];
+                    }
+                }
+            }
+
+            int regions = 0;
+            for (int col = 0; col < 128; col++)
+            {
+                for (int row = 0; row < 128; row++)
+                {
+                    if (grid[col, row] == '#')
+                    {
+                        EliminateRegion(grid, col, row);
+                        regions++;
+                    }
+                }
             }
 
             sw.Stop();
-            output.WriteAnswers(sum, null, sw);
+            output.WriteAnswers(totalBitsSet, regions, sw);
             return output;
         }
 
-        internal static int CountBitsSet(byte[] bytes)
+        private void EliminateRegion(char[,] grid, int col, int row)
         {
-            int sum = 0;
-            foreach (byte b in bytes)
+            grid[col, row] = 'X';
+            foreach ((int, int) dir in directions)
             {
-                sum += bitsSet[b & 0b1111] + bitsSet[b >> 4];
+                int x = col + dir.Item1;
+                int y = row + dir.Item2;
+                if (x < 0 || y < 0 || x >= 128 || y >= 128)
+                {
+                    continue;
+                }
+                if (grid[x, y] == '#')
+                {
+                    EliminateRegion(grid, x, y);
+                }
             }
-            return sum;
         }
     }
 }
