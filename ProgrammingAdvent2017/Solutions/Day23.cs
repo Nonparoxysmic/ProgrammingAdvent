477d99d087d41d6e9c051173c31b00856699b8e5
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-03-17 23:49:55 -0500

    Complete Day 23

--- ProgrammingAdvent2017/Solutions/Day23.cs
+++ ProgrammingAdvent2017/Solutions/Day23.cs
@@ -3,6 +3,7 @@
 // for Advent of Code 2017
 // https://adventofcode.com/2017
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
@@ -50,9 +51,98 @@ namespace ProgrammingAdvent2017.Solutions
             Day23Program program = new Day23Program(instructions);
             long partOneAnswer = program.Run();
 
+            if (!TryOptimize(instructions, out long partTwoAnswer))
+            {
+                output.WriteAnswers(partOneAnswer, "ERROR: Unable to optimize program.", sw);
+                return output;
+            }
+
             sw.Stop();
-            output.WriteAnswers(partOneAnswer, null, sw);
+            output.WriteAnswers(partOneAnswer, partTwoAnswer, sw);
             return output;
+        }
+
+        private bool TryOptimize(Day23Instruction[] instructions, out long result)
+        {
+            if (instructions.Length != 32
+                || !Regex.IsMatch(instructions[0].ToString(), @"^set b \d+$")
+                || instructions[1].ToString() != "set c b"
+                || instructions[2].ToString() != "jnz a 2"
+                || instructions[3].ToString() != "jnz 1 5"
+                || instructions[4].ToString() != "mul b 100"
+                || instructions[5].ToString() != "sub b -100000"
+                || instructions[6].ToString() != "set c b"
+                || instructions[7].ToString() != "sub c -17000"
+                || instructions[8].ToString() != "set f 1"
+                || instructions[9].ToString() != "set d 2"
+                || instructions[10].ToString() != "set e 2"
+                || instructions[11].ToString() != "set g d"
+                || instructions[12].ToString() != "mul g e"
+                || instructions[13].ToString() != "sub g b"
+                || instructions[14].ToString() != "jnz g 2"
+                || instructions[15].ToString() != "set f 0"
+                || instructions[16].ToString() != "sub e -1"
+                || instructions[17].ToString() != "set g e"
+                || instructions[18].ToString() != "sub g b"
+                || instructions[19].ToString() != "jnz g -8"
+                || instructions[20].ToString() != "sub d -1"
+                || instructions[21].ToString() != "set g d"
+                || instructions[22].ToString() != "sub g b"
+                || instructions[23].ToString() != "jnz g -13"
+                || instructions[24].ToString() != "jnz f 2"
+                || instructions[25].ToString() != "sub h -1"
+                || instructions[26].ToString() != "set g b"
+                || instructions[27].ToString() != "sub g c"
+                || instructions[28].ToString() != "jnz g 2"
+                || instructions[29].ToString() != "jnz 1 3"
+                || instructions[30].ToString() != "sub b -17"
+                || instructions[31].ToString() != "jnz 1 -23")
+            {
+                result = 0;
+                return false;
+            }
+
+            int inputValue = instructions[0].Parameter2.Value;
+            int b = 100 * inputValue + 100000;
+            result = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (!IsPrime(b))
+                {
+                    result++;
+                }
+                b += 17;
+            }
+            return true;
+        }
+
+        internal static bool IsPrime(int number)
+        {
+            if (number < 2) { return false; }
+            if (number < 100)
+            {
+                int[] first25Primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23,
+                    29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+                foreach (int prime in first25Primes)
+                {
+                    if (number == prime) { return true; }
+                }
+                return false;
+            }
+
+            if (number % 2 == 0 || number % 5 == 0) { return false; }
+            int boundary = (int)Math.Sqrt(number);
+            for (int i = 3; i <= boundary; i += 10)
+            {
+                if (number % i == 0 ||
+                    number % (i + 4) == 0 ||
+                    number % (i + 6) == 0 ||
+                    number % (i + 8) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         private class Day23Program
