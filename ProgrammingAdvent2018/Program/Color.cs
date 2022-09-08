858e67805f991d9b5fe41460c0ae18615d9e0d6b
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-09-07 23:37:03 -0500

    Add classes for creating bitmaps

---
+++ ProgrammingAdvent2018/Program/Color.cs
@@ -0,0 +1,47 @@
+﻿// ProgrammingAdvent2018 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2018
+// https://adventofcode.com/2018
+
+using System;
+
+namespace ProgrammingAdvent2018.Program
+{
+    public struct Color
+    {
+        public byte A { get; }
+        public byte R { get; }
+        public byte G { get; }
+        public byte B { get; }
+
+        public static Color Black { get => new Color(255, 0, 0, 0); }
+        public static Color White { get => new Color(255, 255, 255, 255); }
+
+        private Color(int alpha, int red, int green, int blue)
+        {
+            A = (byte)alpha;
+            R = (byte)red;
+            G = (byte)green;
+            B = (byte)blue;
+        }
+
+        public static Color FromArgb(int red, int green, int blue)
+        {
+            if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255)
+            {
+                throw new ArgumentException();
+            }
+            return new Color(255, red, green, blue);
+        }
+
+        public static Color FromArgb(int alpha, int red, int green, int blue)
+        {
+            if (alpha < 0 || alpha > 255 || red < 0 || red > 255
+                || green < 0 || green > 255 || blue < 0 || blue > 255)
+            {
+                throw new ArgumentException();
+            }
+            return new Color(alpha, red, green, blue);
+        }
+    }
+}
---
+++ ProgrammingAdvent2018/Program/SimpleBitmap.cs
@@ -0,0 +1,66 @@
+﻿// ProgrammingAdvent2018 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2018
+// https://adventofcode.com/2018
+
+using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ProgrammingAdvent2018.Program
+{
+    public class SimpleBitmap
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        private readonly byte[] pixelData;
+
+        public SimpleBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            pixelData = new byte[4 * width * height];
+        }
+
+        public BitmapSource ToBitmapSource()
+        {
+            return BitmapSource.Create(
+                Width, 
+                Height, 
+                96, 
+                96, 
+                PixelFormats.Bgra32, 
+                null, 
+                pixelData, 
+                4 * Width);
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int index = (Width * y + x) * 4;
+            return Color.FromArgb(
+                pixelData[index + 3],
+                pixelData[index + 2],
+                pixelData[index + 1],
+                pixelData[index]);
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+            {
+                return;
+            }
+            int index = (Width * y + x) * 4;
+            pixelData[index + 3] = color.A;
+            pixelData[index + 2] = color.R;
+            pixelData[index + 1] = color.G;
+            pixelData[index] = color.B;
+        }
+    }
+}
