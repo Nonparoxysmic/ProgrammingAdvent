2c96a6e09be0d6554366792d6ad2c5f384cf5d67
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-09-26 23:43:24 -0500

    Restart Day 15

--- ProgrammingAdvent2018/Solutions/Day15.cs
+++ ProgrammingAdvent2018/Solutions/Day15.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text.RegularExpressions;
 using ProgrammingAdvent2018.Program;
 
@@ -21,21 +22,19 @@ namespace ProgrammingAdvent2018.Solutions
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            if (input.Trim() == "")
-            {
-                output.WriteError("No input.", sw);
-                return output;
-            }
             string[] inputLines = input.ToLines();
 
-            if (!ReadInput(inputLines, out MapArray<char> map, out List<Unit> units, out string message1))
+            if (!ValidateInput(inputLines, out string inputError))
             {
-                output.WriteError(message1, sw);
+                output.WriteError(inputError, sw);
                 return output;
             }
-            if (!CalculateOutcome(map, units, out int partOneAnswer, out string message2))
+            List<Unit> units = new List<Unit>();
+            BattleSquare[,] map = InputToMap(inputLines, units);
+            Battle battle = new Battle(map, units);
+            if (!battle.CalculateOutcome(out int partOneAnswer, out string battleError))
             {
-                output.WriteError(message2, sw);
+                output.WriteError(battleError, sw);
                 return output;
             }
 
@@ -44,17 +43,20 @@ namespace ProgrammingAdvent2018.Solutions
             return output;
         }
 
-        private bool ReadInput(string[] inputLines,
-            out MapArray<char> map, out List<Unit> units, out string message)
+        private bool ValidateInput(string[] inputLines, out string message)
         {
-            units = new List<Unit>();
-            if (inputLines.Length == 0 || inputLines[0].Length == 0)
+            if (inputLines.Length == 0)
             {
-                map = MapArray<char>.Empty;
-                message = "Invalid input.";
+                message = "No input.";
                 return false;
             }
-            map = new MapArray<char>(inputLines[0].Length, inputLines.Length, 1, '#');
+            if (inputLines[0].Length == 0)
+            {
+                message = "First line is empty.";
+                return false;
+            }
+            int goblinUnits = 0;
+            int elfUnits = 0;
             for (int y = 0; y < inputLines.Length; y++)
             {
                 if (inputLines[y].Length != inputLines[0].Length)
@@ -70,65 +72,124 @@ namespace ProgrammingAdvent2018.Solutions
                 }
                 for (int x = 0; x < inputLines[y].Length; x++)
                 {
-                    map[x, y] = inputLines[y][x];
-                    if (map[x, y] == 'G' || map[x, y] == 'E')
+                    if (inputLines[y][x] == 'G')
                     {
-                        units.Add(new Unit(map[x, y], x, y));
+                        goblinUnits++;
+                    }
+                    if (inputLines[y][x] == 'E')
+                    {
+                        elfUnits++;
                     }
                 }
             }
-            if (units.Count == 0)
+            if (goblinUnits + elfUnits == 0)
             {
-                message = "No combat units on the map";
+                message = "No combat units on the map.";
+                return false;
+            }
+            if (goblinUnits == 0)
+            {
+                message = "No Goblin combat units on the map.";
+                return false;
+            }
+            if (elfUnits == 0)
+            {
+                message = "No Elf combat units on the map.";
                 return false;
             }
             message = string.Empty;
             return true;
         }
 
-        private bool CalculateOutcome(MapArray<char> map, List<Unit> units, out int answer, out string message)
+        private BattleSquare[,] InputToMap(string[] inputLines, List<Unit> units)
         {
-            int roundLimit = 60;
-            answer = -1;
-            for (int round = 0; round < roundLimit; round++)
+            BattleSquare[,] map = new BattleSquare[inputLines[0].Length, inputLines.Length];
+            for (int y = 0; y < inputLines.Length; y++)
             {
-                units.Sort();
-                foreach (Unit unit in units)
+                for (int x = 0; x < inputLines[y].Length; x++)
                 {
-                    // TODO: Unit acts.
-                }
-                units.RemoveAll(u => u.IsDead);
-
-                // TODO: Check if combat has ended. Set answer and break if so.
-            }
-            if (answer < 0)
-            {
-                message = $"Combat did not end after {roundLimit} rounds.";
-                return false;
-            }
-            message = string.Empty;
-            return true;
-        }
-
-        internal void Debug_DrawMap(MapArray<char> map)
-        {
-            Debug.WriteLine("");
-            for (int y = 0 - map.Border; y < map.Height + map.Border; y++)
-            {
-                for (int x = 0 - map.Border; x < map.Width + map.Border; x++)
-                {
-                    if (char.IsControl(map[x, y]))
+                    switch (inputLines[y][x])
                     {
-                        Debug.Write(' ');
+                        case '.':
+                            map[x, y] = new BattleSquare(x, y, null);
+                            break;
+                        case 'G':
+                        case 'E':
+                            Unit newUnit = new Unit(inputLines[y][x], x, y);
+                            units.Add(newUnit);
+                            map[x, y] = new BattleSquare(x, y, newUnit);
+                            break;
+                        case '#':
+                        default:
+                            break;
+                    }
+                }
+            }
+            for (int y = 0; y < inputLines.Length - 1; y++)
+            {
+                for (int x = 0; x < inputLines[y].Length - 1; x++)
+                {
+                    BattleSquare current = map[x, y];
+                    if (current == null)
+                    {
+                        continue;
+                    }
+                    BattleSquare right = map[x + 1, y];
+                    if (right != null)
+                    {
+                        current.AdjacentSquares.Add(right);
+                        right.AdjacentSquares.Add(current);
+                    }
+                    BattleSquare down = map[x, y + 1];
+                    if (down != null)
+                    {
+                        current.AdjacentSquares.Add(down);
+                        down.AdjacentSquares.Add(current);
+                    }
+                }
+            }
+            for (int y = 0; y < inputLines.Length; y++)
+            {
+                for (int x = 0; x < inputLines[y].Length; x++)
+                {
+                    if (map[x, y] != null)
+                    {
+                        map[x, y].AdjacentSquares.Sort();
+                    }
+                }
+            }
+            return map;
+        }
+
+        internal void Debug_DrawMap(BattleSquare[,] map)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    if (map[x, y] == null)
+                    {
+                        Debug.Write('#');
+                    }
+                    else if (map[x, y].Occupant == null)
+                    {
+                        Debug.Write('.');
+                    }
+                    else if (map[x, y].Occupant.Type == UnitType.Goblin)
+                    {
+                        Debug.Write('G');
+                    }
+                    else if (map[x, y].Occupant.Type == UnitType.Elf)
+                    {
+                        Debug.Write('E');
                     }
                     else
                     {
-                        Debug.Write(map[x, y]);
+                        Debug.Write('?');
                     }
                 }
                 Debug.WriteLine("");
             }
-            Debug.WriteLine("");
         }
 
         internal void Debug_Tests()
@@ -147,14 +208,17 @@ namespace ProgrammingAdvent2018.Solutions
                 string exampleInput = examples[i].Item1;
                 int exampleOutput = examples[i].Item2;
                 string[] lines = exampleInput.ToLines();
-                if (!ReadInput(lines, out MapArray<char> map, out List<Unit> units, out string message1))
+                if (!ValidateInput(lines, out string inputError))
                 {
-                    Debug.WriteLine($"DEBUG {i}: " + message1);
+                    Debug.WriteLine($"DEBUG {i}: " + inputError);
                     continue;
                 }
-                if (!CalculateOutcome(map, units, out int calculatedAnswer, out string message2))
+                List<Unit> units = new List<Unit>();
+                BattleSquare[,] map = InputToMap(lines, units);
+                Battle battle = new Battle(map, units);
+                if (!battle.CalculateOutcome(out int calculatedAnswer, out string battleError))
                 {
-                    Debug.WriteLine($"DEBUG {i}: " + message2);
+                    Debug.WriteLine($"DEBUG {i}: " + battleError);
                     continue;
                 }
                 if (calculatedAnswer == exampleOutput)
@@ -168,18 +232,40 @@ namespace ProgrammingAdvent2018.Solutions
             }
         }
 
-        private class Unit : IComparable
+        internal enum UnitType
         {
+            None,
+            Goblin,
+            Elf
+        }
+
+        internal class Unit : IComparable
+        {
+            public UnitType Type { get; private set; }
+            public UnitType EnemyType { get; private set; }
             public bool IsDead { get; set; }
-            public char Type { get; private set; }
             public int HP { get; set; }
             public int X { get; set; }
             public int Y { get; set; }
 
             public Unit(char type, int x, int y)
             {
+                if (type == 'G')
+                {
+                    Type = UnitType.Goblin;
+                    EnemyType = UnitType.Elf;
+                }
+                else if (type == 'E')
+                {
+                    Type = UnitType.Elf;
+                    EnemyType = UnitType.Goblin;
+                }
+                else
+                {
+                    Type = UnitType.None;
+                    EnemyType = UnitType.None;
+                }
                 IsDead = false;
-                Type = type;
                 HP = 200;
                 X = x;
                 Y = y;
@@ -203,6 +289,129 @@ namespace ProgrammingAdvent2018.Solutions
                     }
                 }
                 return 1;
+            }
+        }
+
+        internal class BattleSquare : IComparable
+        {
+            public int X { get; private set; }
+            public int Y { get; private set; }
+            public Unit Occupant { get; set; }
+            public List<BattleSquare> AdjacentSquares { get; private set; }
+
+            public BattleSquare(int x, int y, Unit occupant)
+            {
+                X = x;
+                Y = y;
+                Occupant = occupant;
+                AdjacentSquares = new List<BattleSquare>(4);
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj == null)
+                {
+                    return -1;
+                }
+                if (obj is BattleSquare otherNode)
+                {
+                    if (X == otherNode.X && Y == otherNode.Y)
+                    {
+                        return 0;
+                    }
+                    if (Y < otherNode.Y || (Y == otherNode.Y && X < otherNode.X))
+                    {
+                        return -1;
+                    }
+                }
+                return 1;
+            }
+        }
+
+        internal class Battle
+        {
+            private readonly BattleSquare[,] _map;
+            private readonly List<Unit> _units;
+
+            public Battle(BattleSquare[,] map, List<Unit> units)
+            {
+                _map = map;
+                _units = units;
+            }
+
+            public bool CalculateOutcome(out int answer, out string errorMessage)
+            {
+                int roundLimit = 60;
+                List<Unit> goblins = _units.Where(u => u.Type == UnitType.Goblin).ToList();
+                List<Unit> elves = _units.Where(u => u.Type == UnitType.Elf).ToList();
+                answer = -1;
+                for (int round = 0; round < roundLimit; round++)
+                {
+                    foreach (Unit unit in _units)
+                    {
+                        // If this unit is dead, skip to next unit.
+                        if (unit.IsDead)
+                        {
+                            continue;
+                        }
+
+                        // Identify all possible targets.
+                        List<Unit> targets;
+                        if (unit.Type == UnitType.Goblin)
+                        {
+                            targets = elves;
+                        }
+                        else
+                        {
+                            targets = goblins;
+                        }
+
+                        // If no targets remain, combat ends. Calculate answer and return.
+                        if (targets.Count == 0)
+                        {
+                            answer = 0;
+                            foreach (Unit remainingUnit in _units)
+                            {
+                                if (!remainingUnit.IsDead)
+                                {
+                                    answer += remainingUnit.HP;
+                                }
+                            }
+                            answer *= round;
+                            errorMessage = string.Empty;
+                            return true;
+                        }
+
+                        // Identify all of the open squares that are adjacent to any target.
+
+                        // If the unit is not already adjacent to a target, and there are
+                        // no open squares which are adjacent to a target, the unit ends its turn.
+
+                        // If not adjacent to a target, move.
+
+                        //     Identify the nearest open square adjacent to a target. (Usual tie-breaker)
+
+                        //     Take one step toward the chosen square along the shortest path.
+                        //     (Usual tie-breaker for first step)
+
+                        // Then, if adjacent to a target, attack adjacent target with
+                        // the fewest hit points. (Usual tie-breaker)
+
+                        //     Deal damage equal to attack power to the selected target.
+                        //     If this reduces its hit points to 0 or fewer, the selected target dies.
+
+                        targets.RemoveAll(u => u.IsDead);
+                    }
+                    _units.RemoveAll(u => u.IsDead);
+                    _units.Sort();
+                }
+                if (answer < 0)
+                {
+                    errorMessage = $"Combat did not end after {roundLimit} rounds.";
+                    return false;
+                }
+                errorMessage = string.Empty;
+                return true;
             }
         }
     }
