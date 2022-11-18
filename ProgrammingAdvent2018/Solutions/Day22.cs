57a8708132bf18962d23ca618e13eea864037720
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-11-17 22:37:52 -0600

    Attempt Day 22 Part Two and fail

--- ProgrammingAdvent2018/Solutions/Day22.cs
+++ ProgrammingAdvent2018/Solutions/Day22.cs
@@ -3,6 +3,8 @@
 // for Advent of Code 2018
 // https://adventofcode.com/2018
 
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using ProgrammingAdvent2018.Program;
@@ -11,6 +13,14 @@ namespace ProgrammingAdvent2018.Solutions
 {
     internal class Day22 : Day
     {
+        static readonly (int Δx, int Δy)[] adjacentDirections = new (int, int)[]
+        {
+            (-1,  0),
+            ( 1,  0),
+            ( 0, -1),
+            ( 0,  1)
+        };
+
         private readonly Regex depthLine = new Regex(@"depth: ([0-9]{1,9})");
         private readonly Regex targetLine = new Regex(@"target: ([0-9]{1,4}),([0-9]{1,4})");
 
@@ -49,9 +59,12 @@ namespace ProgrammingAdvent2018.Solutions
 
             int partOneAnswer = TotalRiskLevel(regions);
 
+            ConvertToBitFlags(ref regions);
+
+            int partTwoAnswer = ShortestTimeToTarget(regions);
 
             sw.Stop();
-            output.WriteAnswers(partOneAnswer, null, sw);
+            output.WriteAnswers(partOneAnswer, partTwoAnswer, sw);
             return output;
         }
 
@@ -105,6 +118,192 @@ namespace ProgrammingAdvent2018.Solutions
                 }
             }
             return riskSum;
+        }
+
+        private void ConvertToBitFlags(ref MapArray<int> regions)
+        {
+            for (int y = 0; y < regions.Height; y++)
+            {
+                for (int x = 0; x < regions.Width; x++)
+                {
+                    switch (regions[x, y])
+                    {
+                        case 0:
+                            regions[x, y] = 6;
+                            break;
+                        case 1:
+                            regions[x, y] = 5;
+                            break;
+                        case 2:
+                            regions[x, y] = 3;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private int ShortestTimeToTarget(MapArray<int> regions)
+        {
+            SearchNode.Target = (targetX, targetY);
+            List<SearchNode> visitedNodes = new List<SearchNode>();
+            List<SearchNode> seenNodes = new List<SearchNode>
+            {
+                new SearchNode(0, 0, Tool.Torch, 0)
+            };
+            SearchNode currentNode = seenNodes[0];
+            while (true)
+            {
+                // If there are no more nodes to search, return an error code.
+                if (seenNodes.Count == 0)
+                {
+                    return -1;
+                }
+
+                // Select the unvisited node with the shortest tentative travel time.
+                int shortestTime = int.MaxValue;
+                foreach (SearchNode node in seenNodes)
+                {
+                    if (node.Time < shortestTime)
+                    {
+                        currentNode = node;
+                        shortestTime = node.Time;
+                    }
+                }
+
+                // If the target has been reached, return the travel time.
+                if (currentNode.IsTarget())
+                {
+                    return currentNode.Time;
+                }
+
+                // Consider all unvisited neighbors and update their tentative times.
+                foreach ((int Δx, int Δy) in adjacentDirections)
+                {
+                    if ((currentNode.Tool & regions[currentNode.X + Δx, currentNode.Y + Δy]) == 0)
+                    {
+                        continue;
+                    }
+                    SearchNode neighborNode = new SearchNode(currentNode.X + Δx, currentNode.Y + Δy, currentNode.Tool, currentNode.Time + 1);
+                    if (visitedNodes.Contains(neighborNode))
+                    {
+                        continue;
+                    }
+                    if (!seenNodes.Contains(neighborNode))
+                    {
+                        seenNodes.Add(neighborNode);
+                        continue;
+                    }
+                    for (int i = 0; i < seenNodes.Count; i++)
+                    {
+                        SearchNode node = seenNodes[i];
+                        if (node == neighborNode)
+                        {
+                            node.Time = Math.Min(node.Time, currentNode.Time + 1);
+                            break;
+                        }
+                    }
+                }
+                foreach (int tool in Enum.GetValues(typeof(Tool)))
+                {
+                    if (tool == currentNode.Tool)
+                    {
+                        continue;
+                    }
+                    if ((tool & regions[currentNode.X, currentNode.Y]) == 0)
+                    {
+                        continue;
+                    }
+                    SearchNode neighborNode = new SearchNode(currentNode.X, currentNode.Y, tool, currentNode.Time + 7);
+                    if (visitedNodes.Contains(neighborNode))
+                    {
+                        break;
+                    }
+                    if (!seenNodes.Contains(neighborNode))
+                    {
+                        seenNodes.Add(neighborNode);
+                        break;
+                    }
+                    for (int i = 0; i < seenNodes.Count; i++)
+                    {
+                        SearchNode node = seenNodes[i];
+                        if (node == neighborNode)
+                        {
+                            node.Time = Math.Min(node.Time, currentNode.Time + 7);
+                            break;
+                        }
+                    }
+                    break;
+                }
+
+                // Mark the current node as visited.
+                visitedNodes.Add(currentNode);
+                seenNodes.Remove(currentNode);
+            }
+        }
+
+        private struct SearchNode : IEquatable<SearchNode>
+        {
+            public static (int X, int Y) Target { get; set; }
+
+            public int X { get; private set; }
+            public int Y { get; private set; }
+            public int Tool { get; private set; }
+            public int Time { get; set; }
+
+            public SearchNode(int x, int y, Tool tool, int time)
+            {
+                X = x;
+                Y = y;
+                Tool = (int)tool;
+                Time = time;
+            }
+
+            public SearchNode(int x, int y, int tool, int time)
+            {
+                X = x;
+                Y = y;
+                Tool = tool;
+                Time = time;
+            }
+
+            public bool IsTarget()
+            {
+                if (X == Target.X && Y == Target.Y && Tool == 2)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            bool IEquatable<SearchNode>.Equals(SearchNode other)
+            {
+                if (X == other.X && Y == other.Y && Tool == other.Tool)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public static bool operator ==(SearchNode a, SearchNode b) => a.Equals(b);
+
+            public static bool operator !=(SearchNode a, SearchNode b) => !a.Equals(b);
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+
+        private enum Tool
+        {
+            None = 1,
+            Torch = 2,
+            Gear = 4
         }
     }
 }
