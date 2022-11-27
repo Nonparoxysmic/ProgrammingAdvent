1164ab6ce483d464b8fa3571bf94a1fde4b1e112
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-11-26 23:51:45 -0600

    Complete Day 23

--- ProgrammingAdvent2018/Solutions/Day23.cs
+++ ProgrammingAdvent2018/Solutions/Day23.cs
@@ -15,6 +15,8 @@ namespace ProgrammingAdvent2018.Solutions
     {
         private readonly Regex validLine = new Regex(@"pos=<(-?[0-9]{1,9}),(-?[0-9]{1,9}),(-?[0-9]{1,9})>, r=([0-9]{1,9})");
 
+        private List<(int, int, int, int)> nanobots;
+
         internal override PuzzleAnswers Solve(string input)
         {
             PuzzleAnswers output = new PuzzleAnswers();
@@ -29,7 +31,7 @@ namespace ProgrammingAdvent2018.Solutions
             }
             string[] inputLines = input.ToLines();
 
-            List<(int, int, int, int)> nanobots = new List<(int, int, int, int)>();
+            nanobots = new List<(int, int, int, int)>();
             (int X, int Y, int Z, int R) strongestNanobot = (0, 0, 0, -1);
             for (int i = 0; i < inputLines.Length; i++)
             {
@@ -62,9 +64,67 @@ namespace ProgrammingAdvent2018.Solutions
                 }
             }
 
+            int partTwoAnswer = PartTwoAnswer();
+
             sw.Stop();
-            output.WriteAnswers(partOneAnswer, null, sw);
+            output.WriteAnswers(partOneAnswer, partTwoAnswer, sw);
             return output;
+        }
+
+        private int PartTwoAnswer()
+        {
+            (int X, int Y, int Z) center = AveragePosition();
+            for (int i = 24; i > 0; i--)
+            {
+                int step = (int)Math.Pow(2, i);
+                (int X, int Y, int Z) bestPosition = center;
+                int bestPositionNanobots = -1;
+                for (int z = center.Z - 8 * step; z <= center.Z + 8 * step; z += step)
+                {
+                    for (int y = center.Y - 8 * step; y <= center.Y + 8 * step; y += step)
+                    {
+                        for (int x = center.X - 8 * step; x <= center.X + 8 * step; x += step)
+                        {
+                            int nanobotsInRange = NanobotsInRange(x, y, z);
+                            if (nanobotsInRange > bestPositionNanobots ||
+                                (nanobotsInRange == bestPositionNanobots &&
+                                x + y + z < bestPosition.X + bestPosition.Y + bestPosition.Z))
+                            {
+                                bestPositionNanobots = nanobotsInRange;
+                                bestPosition = (x, y, z);
+                            }
+                        }
+                    }
+                }
+                center = bestPosition;
+            }
+            return center.X + center.Y + center.Z;
+        }
+
+        private (int, int, int) AveragePosition()
+        {
+            int sumX = 0, sumY = 0, sumZ = 0;
+            foreach ((int X, int Y, int Z, int R) in nanobots)
+            {
+                sumX += X;
+                sumY += Y;
+                sumZ += Z;
+            }
+            return (sumX / nanobots.Count, sumY / nanobots.Count, sumZ / nanobots.Count);
+        }
+
+        private int NanobotsInRange(int x, int y, int z)
+        {
+            int count = 0;
+            foreach ((int X, int Y, int Z, int R) in nanobots)
+            {
+                int distance = Math.Abs(X - x) + Math.Abs(Y - y) + Math.Abs(Z - z);
+                if (distance <= R)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
