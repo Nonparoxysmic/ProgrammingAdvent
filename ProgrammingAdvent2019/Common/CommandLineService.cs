570a48b48dd31cb17eed5a5600522a4e878159e2
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-04-07 17:50:52 -0500

    Add command line interface

---
+++ ProgrammingAdvent2019/Common/CommandLineService.cs
@@ -0,0 +1,51 @@
+﻿// ProgrammingAdvent2019 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2019
+// https://adventofcode.com/2019
+
+using Microsoft.Extensions.Hosting;
+using ProgrammingAdvent2019.Solutions;
+
+namespace ProgrammingAdvent2019.Common;
+
+internal class CommandLineService : BackgroundService
+{
+    private readonly IHostApplicationLifetime _applicationLifetime;
+
+    public CommandLineService(IHostApplicationLifetime applicationLifetime)
+    {
+        _applicationLifetime = applicationLifetime;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            ExecuteCommandLineInterface(stoppingToken);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{ex.GetType()}: {ex.Message}");
+            Console.WriteLine("The application will now terminate.");
+            Console.ResetColor();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+        _applicationLifetime.StopApplication();
+        return Task.CompletedTask;
+    }
+
+    private static void ExecuteCommandLineInterface(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine() ?? string.Empty;
+            if (input == "exit")
+            {
+                break;
+            }
+        }
+    }
+}
--- ProgrammingAdvent2019/Program.cs
+++ ProgrammingAdvent2019/Program.cs
@@ -3,5 +3,17 @@
 // for Advent of Code 2019
 // https://adventofcode.com/2019
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProgrammingAdvent2019.Common;
+
+Console.Title = "ProgrammingAdvent2019 by Nonparoxysmic";
+
+IHost host = new HostBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<CommandLineService>();
+    })
+    .Build();
+
+await host.RunAsync();
