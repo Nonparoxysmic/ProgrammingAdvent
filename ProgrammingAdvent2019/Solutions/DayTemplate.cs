82c216582d31bc8b7b9e927ba5a1350892650e55
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-04-08 13:52:11 -0500

    Add template for puzzle solutions

---
+++ ProgrammingAdvent2019/Solutions/DayTemplate.cs
@@ -0,0 +1,23 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2019
+// https://adventofcode.com/2019
+
+using ProgrammingAdvent2019.Common;
+
+namespace ProgrammingAdvent2019.Solutions;
+
+internal class DayTemplate : Day
+{
+    public override bool ValidateInput(string[] inputLines, out string errorMessage)
+    {
+        errorMessage = "Input validation not yet implemented.";
+        return false;
+    }
+
+    protected override PuzzleAnswers CalculateAnswers(string[] inputLines)
+    {
+        PuzzleAnswers output = new();
+        return output.WriteAnswers(null, null);
+    }
+}
