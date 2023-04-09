a590c5a6b9aff30e10e57fc5832be838c1922224
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-04-09 09:30:27 -0500

    Add input text files

---
+++ ProgrammingAdvent2019/Common/SystemIO.cs
@@ -0,0 +1,29 @@
+﻿// ProgrammingAdvent2019 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2019
+// https://adventofcode.com/2019
+
+namespace ProgrammingAdvent2019.Common;
+
+internal static class SystemIO
+{
+    public static bool TryReadInputFile(int dayNumber, out string[] lines)
+    {
+        string path = $"InputFiles\\InputDay{dayNumber:00}.txt";
+        if (dayNumber < 1 || dayNumber > 25 || !File.Exists(path))
+        {
+            lines = Array.Empty<string>();
+            return false;
+        }
+        try
+        {
+            lines = File.ReadAllLines(path);
+        }
+        catch
+        {
+            lines = Array.Empty<string>();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ ProgrammingAdvent2019/InputFiles/Readme.txt
@@ -0,0 +1,15 @@
+ProgrammingAdvent2019\InputFiles\
+
+ProgrammingAdvent2019 by Nonparoxysmic
+https://github.com/Nonparoxysmic/ProgrammingAdvent
+
+This program reads text files in this folder
+that contain puzzle inputs for Advent of Code 2019.
+See the Advent of Code 2019 website for format information.
+https://adventofcode.com/2019
+
+File Name Format:
+InputDay01.txt
+InputDay02.txt
+InputDay03.txt
+etc.
