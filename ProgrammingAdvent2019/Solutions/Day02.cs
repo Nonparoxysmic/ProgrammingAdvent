0e3b8e1956cd8b506cba75721819ca2012bca97b
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-04-13 07:38:39 -0500

    Complete Day 2

--- ProgrammingAdvent2019/Solutions/Day02.cs
+++ ProgrammingAdvent2019/Solutions/Day02.cs
@@ -49,51 +49,60 @@ internal class Day02 : Day
         int[] input = inputLines.First(s => s.Length > 0).Split(',').Select(s => int.Parse(s)).ToArray();
         input[1] = 12;
         input[2] = 2;
-        if (!RunProgram(input, out string errorMessage))
+        if (!RunProgram(input, out int partOneAnswer, out string error1))
         {
-            output.WriteError(errorMessage);
+            output.WriteError(error1);
             return output;
         }
-        return output.WriteAnswers(input[0], null);
+        if (!PartTwoAnswer(input, out int partTwoAnswer, out string error2))
+        {
+            output.WriteError(error2);
+            return output;
+        }
+        return output.WriteAnswers(partOneAnswer, partTwoAnswer);
     }
 
-    private static bool RunProgram(int[] input, out string errorMessage)
+    private static bool RunProgram(int[] input, out int result, out string errorMessage)
     {
+        result = -1;
+        int[] program = new int[input.Length];
+        Array.Copy(input, program, input.Length);
         int position = 0;
         while (true)
         {
-            if (position >= input.Length)
+            if (position >= program.Length)
             {
                 errorMessage = "Program counter went out of bounds.";
                 return false;
             }
-            if (input[position] == 99)
+            if (program[position] == 99)
             {
                 errorMessage = string.Empty;
+                result = program[0];
                 return true;
             }
-            if (position > input.Length - 4)
+            if (position > program.Length - 4)
             {
                 errorMessage = "Unknown error.";
                 return false;
             }
-            int pos1 = input[position + 1];
-            int pos2 = input[position + 2];
-            int pos3 = input[position + 3];
-            if (pos1 < 0 || pos1 >= input.Length ||
-                pos2 < 0 || pos2 >= input.Length ||
-                pos3 < 0 || pos3 >= input.Length)
+            int pos1 = program[position + 1];
+            int pos2 = program[position + 2];
+            int pos3 = program[position + 3];
+            if (pos1 < 0 || pos1 >= program.Length ||
+                pos2 < 0 || pos2 >= program.Length ||
+                pos3 < 0 || pos3 >= program.Length)
             {
                 errorMessage = "Encountered invalid position data.";
                 return false;
             }
-            if (input[position] == 1)
+            if (program[position] == 1)
             {
-                input[pos3] = input[pos1] + input[pos2];
+                program[pos3] = program[pos1] + program[pos2];
             }
-            else if (input[position] == 2)
+            else if (program[position] == 2)
             {
-                input[pos3] = input[pos1] * input[pos2];
+                program[pos3] = program[pos1] * program[pos2];
             }
             else
             {
@@ -102,5 +111,26 @@ internal class Day02 : Day
             }
             position += 4;
         }
+    }
+
+    private static bool PartTwoAnswer(int[] input, out int answer, out string errorMessage)
+    {
+        for (int noun = 0; noun < 100; noun++)
+        {
+            for (int verb = 0; verb < 100; verb++)
+            {
+                input[1] = noun;
+                input[2] = verb;
+                if (RunProgram(input, out int result, out string _) && result == 19690720)
+                {
+                    errorMessage = string.Empty;
+                    answer = 100 * noun + verb;
+                    return true;
+                }
+            }
+        }
+        errorMessage = "No valid Part Two answer exists.";
+        answer = -1;
+        return false;
     }
 }
