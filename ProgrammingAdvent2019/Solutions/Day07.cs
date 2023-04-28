2e14999a3ace06b695e5f2298a2989acf4d653df
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-04-28 07:42:26 -0500

    Complete Day 7

--- ProgrammingAdvent2019/Solutions/Day07.cs
+++ ProgrammingAdvent2019/Solutions/Day07.cs
@@ -45,61 +45,230 @@ internal class Day07 : Day
     protected override PuzzleAnswers CalculateAnswers(string[] inputLines)
     {
         PuzzleAnswers output = new();
+
+        // Part One
         Permutatable<int> settingPermutations = new(new int[] { 0, 1, 2, 3, 4 });
         int highestSignal = int.MinValue;
         foreach (int[] permutation in settingPermutations)
         {
-            if (!RunProgramSeries(inputLines[0], permutation, out int result))
+            if (!RunProgramSeries(inputLines[0], permutation, out int result, out string error))
             {
-                return output.WriteError("");
+                return output.WriteError($"Part One: A program encountered an error. {error}");
             }
             highestSignal = Math.Max(highestSignal, result);
         }
-        return output.WriteAnswers(highestSignal, null);
+        // Part Two
+        settingPermutations = new(new int[] { 5, 6, 7, 8, 9 });
+        int highestFeedbackSignal = int.MinValue;
+        foreach (int[] permutation in settingPermutations)
+        {
+            if (!RunFeedbackSeries(inputLines[0], permutation, out int result, out string error))
+            {
+                return output.WriteError($"Part Two: A program encountered an error. {error}");
+            }
+            highestFeedbackSignal = Math.Max(highestFeedbackSignal, result);
+        }
+
+        return output.WriteAnswers(highestSignal, highestFeedbackSignal);
     }
 
-    private static bool RunProgramSeries(string intcode, int[] sequence, out int output)
+    private static bool RunProgramSeries(string intcode, int[] settings, out int result, out string error)
     {
-        int signal = 0;
-        foreach (int setting in sequence)
+        result = 0;
+        error = string.Empty;
+        Day07Program[] programs = new Day07Program[settings.Length];
+        for (int i = 0; i < programs.Length; i++)
         {
-            if (!RunProgram(intcode, setting, signal, out signal))
+            programs[i] = new(intcode);
+            programs[i].EnqueueInput(settings[i]);
+        }
+        programs[0].EnqueueInput(0);
+        for (int i = 0; i < programs.Length; i++)
+        {
+            while (programs[i].Tick()) { }
+            if (programs[i].Status == Day07Program.ProgramStatus.Error)
             {
-                output = signal;
+                error = programs[i].Error;
                 return false;
             }
+            if (i == programs.Length - 1)
+            {
+                if (programs[i].Status == Day07Program.ProgramStatus.Halted)
+                {
+                    if (programs[i].OutputAvailable())
+                    {
+                        result = programs[i].DequeueOutput();
+                        return true;
+                    }
+                    else
+                    {
+                        error = "Final program halted without output.";
+                        return false;
+                    }
+                }
+                else if (programs[i].OutputAvailable())
+                {
+                    result = programs[i].DequeueOutput();
+                    return true;
+                }
+                else
+                {
+                    error = "Final program failed to complete.";
+                    return false;
+                }
+            }
+            else if (programs[i].OutputAvailable())
+            {
+                programs[i + 1].EnqueueInput(programs[i].DequeueOutput());
+            }
         }
-        output = signal;
-        return true;
+        error = "Unknown error.";
+        return false;
     }
 
-    private static bool RunProgram(string intcode, int input1, int input2, out int output)
+    private static bool RunFeedbackSeries(string intcode, int[] settings, out int result, out string error)
     {
-        output = 0;
-        int input = input1;
-        string[] program = intcode.Split(',');
-        int instructionPointer = 0;
-        while (0 <= instructionPointer && instructionPointer < program.Length)
+        result = 0;
+        error = string.Empty;
+        Day07Program[] programs = new Day07Program[settings.Length];
+        for (int i = 0; i < programs.Length; i++)
         {
-            if (program[instructionPointer][0] == '-')
+            programs[i] = new(intcode);
+            programs[i].EnqueueInput(settings[i]);
+        }
+        programs[0].EnqueueInput(0);
+        int timeout = 0;
+        while (timeout++ < 1_000)
+        {
+            for (int i = 0; i < programs.Length; i++)
             {
-                output = -45;
+                while (programs[i].Tick()) { }
+                if (programs[i].Status == Day07Program.ProgramStatus.Error)
+                {
+                    error = programs[i].Error;
+                    return false;
+                }
+                if (i == programs.Length - 1)
+                {
+                    if (programs[i].Status == Day07Program.ProgramStatus.Halted)
+                    {
+                        if (programs[i].OutputAvailable())
+                        {
+                            result = programs[i].DequeueOutput();
+                            return true;
+                        }
+                        else
+                        {
+                            error = "Final program halted without output.";
+                            return false;
+                        }
+                    }
+                    else if (programs[i].OutputAvailable())
+                    {
+                        programs[0].EnqueueInput(programs[i].DequeueOutput());
+                    }
+                }
+                else if (programs[i].OutputAvailable())
+                {
+                    programs[i + 1].EnqueueInput(programs[i].DequeueOutput());
+                }
+            }
+        }
+        error = "Feedback loop ran too long.";
+        return false;
+    }
+
+    private class Day07Program
+    {
+        private readonly string[] _program;
+        private int _instructionPointer;
+        private readonly Queue<int> _inputs = new();
+        private readonly Queue<int> _outputs = new();
+        private int _ticks;
+
+        public ProgramStatus Status { get; private set; } = ProgramStatus.Running;
+        public string Error { get; private set; } = string.Empty;
+
+        public Day07Program(string intcode)
+        {
+            _program = intcode.Split(',');
+        }
+
+        public void EnqueueInput(int input)
+        {
+            _inputs.Enqueue(input);
+            if (Status == ProgramStatus.Waiting)
+            {
+                Status = ProgramStatus.Running;
+            }
+        }
+
+        public bool OutputAvailable()
+        {
+            return _outputs.Count > 0;
+        }
+
+        public int DequeueOutput()
+        {
+            return _outputs.Dequeue();
+        }
+
+        public bool Tick()
+        {
+            if (_ticks++ > 10_000)
+            {
+                Status = ProgramStatus.Error;
+                Error = "Program ran too long.";
+                return false;
+            }
+            if (Status == ProgramStatus.Error || 
+                Status == ProgramStatus.Waiting ||
+                Status == ProgramStatus.Halted)
+            {
+                return false;
+            }
+            if (_instructionPointer < 0 || _instructionPointer >= _program.Length)
+            {
+                Status = ProgramStatus.Error;
+                Error = "Instruction pointer went out of bounds.";
+                return false;
+            }
+            if (_program[_instructionPointer][0] == '-')
+            {
+                Status = ProgramStatus.Error;
+                Error = $"Encountered a negative instruction code at position {_instructionPointer}.";
                 return false;
             }
             int opcode;
             int[] parameterModes = new int[3];
-            if (program[instructionPointer].Length <= 2)
+            if (_program[_instructionPointer].Length <= 2)
             {
-                opcode = int.Parse(program[instructionPointer]);
+                opcode = int.Parse(_program[_instructionPointer]);
             }
             else
             {
-                string term = program[instructionPointer];
+                string term = _program[_instructionPointer];
                 opcode = 10 * (term[^2] - '0') + term[^1] - '0';
                 for (int i = 0; i < 3 && i < term.Length - 2; i++)
                 {
                     parameterModes[i] = term[term.Length - 3 - i] - '0';
                 }
+            }
+            if (opcode == 99)
+            {
+                Status = ProgramStatus.Halted;
+                return true;
+            }
+            if (opcode < 1 || opcode > 8)
+            {
+                Status = ProgramStatus.Error;
+                Error = $"Invalid opcode {opcode} at position {_instructionPointer}.";
+                return false;
+            }
+            if (opcode == 3 && _inputs.Count == 0)
+            {
+                Status = ProgramStatus.Waiting;
+                return false;
             }
             int stepSize = opcode switch
             {
@@ -113,9 +282,10 @@ internal class Day07 : Day
                 8 => 4,
                 _ => 0
             };
-            if (instructionPointer > program.Length - stepSize)
+            if (_instructionPointer > _program.Length - stepSize)
             {
-                output = int.MinValue;
+                Status = ProgramStatus.Error;
+                Error = $"Instruction with insufficent parameters at position {_instructionPointer}.";
                 return false;
             }
             int[] parameterPositions = new int[stepSize - 1];
@@ -123,87 +293,69 @@ internal class Day07 : Day
             {
                 if (parameterModes[i] == 0)
                 {
-                    parameterPositions[i] = int.Parse(program[instructionPointer + 1 + i]);
-                    if (parameterPositions[i] < 0 || parameterPositions[i] >= program.Length)
+                    parameterPositions[i] = int.Parse(_program[_instructionPointer + 1 + i]);
+                    if (parameterPositions[i] < 0 || parameterPositions[i] >= _program.Length)
                     {
-                        output = -8;
+                        Status = ProgramStatus.Error;
+                        Error = $"Parameter at position {_instructionPointer + 1 + i} points out of bounds.";
                         return false;
                     }
                 }
                 else
                 {
-                    parameterPositions[i] = instructionPointer + 1 + i;
+                    parameterPositions[i] = _instructionPointer + 1 + i;
                 }
             }
-            if (opcode == 99)
-            {
-                output = -99;
-                return false;
-            }
-            if (opcode < 1 || opcode > 8)
-            {
-                output = -400;
-                return false;
-            }
-            if (!DoInstruction(program, ref instructionPointer, opcode, parameterPositions, input, out int result))
-            {
-                output = result;
-                return false;
-            }
-            if (opcode == 4)
-            {
-                output = result;
-                return true;
-            }
-            if (opcode == 3)
-            {
-                input = input2;
-            }
-            instructionPointer += stepSize;
+            DoInstruction(opcode, parameterPositions);
+            _instructionPointer += stepSize;
+            return true;
         }
-        output = -1;
-        return false;
-    }
 
-    private static bool DoInstruction(string[] program, ref int instructionPointer, int opcode, int[] parameterPositions, int input, out int result)
-    {
-        result = 0;
-        int A = int.Parse(program[parameterPositions[0]]);
-        int B = parameterPositions.Length > 1 ? int.Parse(program[parameterPositions[1]]) : 0;
-        switch (opcode)
+        private void DoInstruction(int opcode, int[] parameterPositions)
         {
-            case 1:
-                program[parameterPositions[2]] = (A + B).ToString();
-                return true;
-            case 2:
-                program[parameterPositions[2]] = (A * B).ToString();
-                return true;
-            case 3:
-                program[parameterPositions[0]] = input.ToString();
-                return true;
-            case 4:
-                result = A;
-                return true;
-            case 5:
-                if (A != 0)
-                {
-                    instructionPointer = B - 3;
-                }
-                return true;
-            case 6:
-                if (A == 0)
-                {
-                    instructionPointer = B - 3;
-                }
-                return true;
-            case 7:
-                program[parameterPositions[2]] = A < B ? "1" : "0";
-                return true;
-            case 8:
-                program[parameterPositions[2]] = A == B ? "1" : "0";
-                return true;
+            int A = int.Parse(_program[parameterPositions[0]]);
+            int B = parameterPositions.Length > 1 ? int.Parse(_program[parameterPositions[1]]) : 0;
+            switch (opcode)
+            {
+                case 1:
+                    _program[parameterPositions[2]] = (A + B).ToString();
+                    break;
+                case 2:
+                    _program[parameterPositions[2]] = (A * B).ToString();
+                    break;
+                case 3:
+                    _program[parameterPositions[0]] = _inputs.Dequeue().ToString();
+                    break;
+                case 4:
+                    _outputs.Enqueue(A);
+                    break;
+                case 5:
+                    if (A != 0)
+                    {
+                        _instructionPointer = B - 3;
+                    }
+                    break;
+                case 6:
+                    if (A == 0)
+                    {
+                        _instructionPointer = B - 3;
+                    }
+                    break;
+                case 7:
+                    _program[parameterPositions[2]] = A < B ? "1" : "0";
+                    break;
+                case 8:
+                    _program[parameterPositions[2]] = A == B ? "1" : "0";
+                    break;
+            }
         }
-        result = -100 - opcode;
-        return false;
+
+        public enum ProgramStatus
+        {
+            Error = -1,
+            Waiting,
+            Running,
+            Halted
+        }
     }
 }
