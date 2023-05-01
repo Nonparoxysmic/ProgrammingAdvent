08b79a87acf0373f56b2544799519c4dbd98cded
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-05-01 07:32:24 -0500

    Complete Day 8

--- ProgrammingAdvent2019/Solutions/Day08.cs
+++ ProgrammingAdvent2019/Solutions/Day08.cs
@@ -41,7 +41,7 @@ internal class Day08 : Day
     protected override PuzzleAnswers CalculateAnswers(string[] inputLines)
     {
         PuzzleAnswers output = new();
-        char[,,] pixels = new char[inputLines[0].Length / 150, 25, 6];
+        char[,,] pixelData = new char[inputLines[0].Length / 150, 25, 6];
         int fewestZeroes = int.MaxValue;
         int fewestZeroesLayer = 0;
         for (int layer = 0; layer < inputLines[0].Length / 150; layer++)
@@ -56,7 +56,7 @@ internal class Day08 : Day
                 }
                 int x = i % 25;
                 int y = i / 25;
-                pixels[layer, x, y] = c;
+                pixelData[layer, x, y] = c;
             }
             if (zeroCount < fewestZeroes)
             {
@@ -70,7 +70,7 @@ internal class Day08 : Day
         {
             for (int x = 0; x < 25; x++)
             {
-                char pixel = pixels[fewestZeroesLayer, x, y];
+                char pixel = pixelData[fewestZeroesLayer, x, y];
                 if (pixel == '1')
                 {
                     oneCount++;
@@ -81,6 +81,80 @@ internal class Day08 : Day
                 }
             }
         }
-        return output.WriteAnswers(oneCount * twoCount, null);
+        char[,] mergedLayers = MergeLayers(pixelData);
+        char[] password = new char[mergedLayers.GetLength(0) / 5];
+        bool foundUnknownLetters = false;
+        for (int letter = 0; letter < password.Length; letter++)
+        {
+            char[] pixels = new char[5 * mergedLayers.GetLength(1)];
+            int i = 0;
+            for (int y = 0; y < mergedLayers.GetLength(1); y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    pixels[i++] = mergedLayers[x + 5 * letter, y];
+                }
+            }
+            if (!TryGetLetter(string.Join(null, pixels), out char c))
+            {
+                foundUnknownLetters = true;
+            }
+            password[letter] = c;
+        }
+        if (foundUnknownLetters)
+        {
+            string message = "Unrecognized letter(s) in answer: " + string.Join(null, password);
+            return output.WriteAnswers(oneCount * twoCount, message);
+        }
+        return output.WriteAnswers(oneCount * twoCount, password);
+    }
+
+    private static char[,] MergeLayers(char[,,] pixels)
+    {
+        char[,] output = new char[pixels.GetLength(1), pixels.GetLength(2)];
+        for (int y = 0; y < pixels.GetLength(2); y++)
+        {
+            for (int x = 0; x < pixels.GetLength(1); x++)
+            {
+                char pixel = '2';
+                for (int layer = 0; layer < pixels.GetLength(0); layer++)
+                {
+                    if (pixels[layer, x, y] == '0' || pixels[layer, x, y] == '1')
+                    {
+                        pixel = pixels[layer, x, y];
+                        break;
+                    }
+                }
+                output[x, y] = pixel;
+            }
+        }
+        return output;
+    }
+
+    public static bool TryGetLetter(string pixels, out char letter)
+    {
+        letter = pixels switch
+        {
+            "011001001010010111101001010010" => 'A',
+            "111001001011100100101001011100" => 'B',
+            "011001001010000100001001001100" => 'C',
+            "111101000011100100001000011110" => 'E',
+            "111101000011100100001000010000" => 'F',
+            "011001001010000101101001001110" => 'G',
+            "100101001011110100101001010010" => 'H',
+            "011100010000100001000010001110" => 'I',
+            "001100001000010000101001001100" => 'J',
+            "100101010011000101001010010010" => 'K',
+            "100001000010000100001000011110" => 'L',
+            "011001001010010100101001001100" => 'O',
+            "111001001010010111001000010000" => 'P',
+            "111001001010010111001010010010" => 'R',
+            "011101000010000011000001011100" => 'S',
+            "100101001010010100101001001100" => 'U',
+            "100011000101010001000010000100" => 'Y',
+            "111100001000100010001000011110" => 'Z',
+            _ => '?'
+        };
+        return letter != '?';
     }
 }
