bbe6af4cf9cafaf6580db3ecdc0340d98d311080
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-05-05 07:47:16 -0500

    Complete Day 10

--- ProgrammingAdvent2019/Solutions/Day10.cs
+++ ProgrammingAdvent2019/Solutions/Day10.cs
@@ -3,6 +3,7 @@
 // for Advent of Code 2019
 // https://adventofcode.com/2019
 
+using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
 using ProgrammingAdvent2019.Common;
 
@@ -20,6 +21,7 @@ internal class Day10 : Day
             return false;
         }
         int lineLength = inputLines[0].Length;
+        int asteroidCount = 0;
         foreach (string line in inputLines)
         {
             if (line.Length == 0)
@@ -37,6 +39,18 @@ internal class Day10 : Day
                 errorMessage = $"Input \"{line.Left(20, true)}\" contains invalid characters.";
                 return false;
             }
+            foreach (char c in line)
+            {
+                if (c == '#')
+                {
+                    asteroidCount++;
+                }
+            }
+        }
+        if (asteroidCount == 0)
+        {
+            errorMessage = "No asteroids in the map.";
+            return false;
         }
         errorMessage = string.Empty;
         return true;
@@ -56,13 +70,38 @@ internal class Day10 : Day
                 }
             }
         }
+        if (asteroids.Count == 1 || asteroids.Count == 2)
+        {
+            return output.WriteAnswers(asteroids.Count - 1, "There are not 200 other asteroids.");
+        }
         int bestDetectableAsteroids = 0;
+        Vector2Int bestPosition = Vector2Int.Zero;
         Parallel.ForEach(asteroids, position =>
         {
             int detectableAsteroids = DetectableAsteroids(position.X, position.Y, inputLines, asteroids);
-            bestDetectableAsteroids = Math.Max(bestDetectableAsteroids, detectableAsteroids);
+            if (detectableAsteroids > bestDetectableAsteroids)
+            {
+                bestDetectableAsteroids = detectableAsteroids;
+                bestPosition = position;
+            }
         });
-        return output.WriteAnswers(bestDetectableAsteroids, null);
+        if (asteroids.Count < 201)
+        {
+            return output.WriteAnswers(bestDetectableAsteroids, "There are not 200 other asteroids.");
+        }
+        ConcurrentDictionary<int, (double, int)> asteroidData = new();
+        Parallel.ForEach(asteroids, position =>
+        {
+            if (position == bestPosition)
+            {
+                return;
+            }
+            int key = 100 * position.X + position.Y;
+            asteroidData.TryAdd(key, CalculateAsteroidData(position, bestPosition, inputLines));
+        });
+        List<KeyValuePair<int, (double, int)>> asteroidDataList = asteroidData.ToList();
+        asteroidDataList.Sort(CompareVaporizationOrder);
+        return output.WriteAnswers(bestDetectableAsteroids, asteroidDataList[199].Key);
     }
 
     private static int DetectableAsteroids(int xPos, int yPos, string[] input, List<Vector2Int> asteroids)
@@ -82,7 +121,7 @@ internal class Day10 : Day
             {
                 continue;
             }
-            Vector2Int step = StepVector(asteroid.X - xPos, asteroid.Y - yPos);
+            Vector2Int step = StepVector(asteroid.X - xPos, asteroid.Y - yPos, out int _);
             Vector2Int position = asteroid + step;
             while (position.X >= 0 && position.Y >= 0
                 && position.X < input[0].Length && position.Y < input.Length)
@@ -105,13 +144,52 @@ internal class Day10 : Day
         return detectable;
     }
 
-    private static Vector2Int StepVector(int x, int y)
+    private static Vector2Int StepVector(int x, int y, out int factor)
     {
         int a = Math.Abs(x), b = Math.Abs(y);
         while (b != 0)
         {
             (a, b) = (b, a % b);
         }
+        factor = a;
         return new Vector2Int(x / a, y / a);
+    }
+
+    private static (double, int) CalculateAsteroidData(Vector2Int position, Vector2Int bestPosition, string[] inputLines)
+    {
+        double angle = -Math.Atan2(position.X - bestPosition.X, position.Y - bestPosition.Y);
+        int depth = 0;
+        Vector2Int pos = position;
+        Vector2Int step = StepVector(bestPosition.X - position.X, bestPosition.Y - position.Y, out int factor);
+        for (int i = 0; i < factor - 1; i++)
+        {
+            pos += step;
+            if (inputLines[pos.Y][pos.X] == '#')
+            {
+                depth++;
+            }
+        }
+        return (angle, depth);
+    }
+
+    private int CompareVaporizationOrder(KeyValuePair<int, (double, int)> A, KeyValuePair<int, (double, int)> B)
+    {
+        if (A.Value.Item2 < B.Value.Item2)
+        {
+            return -1;
+        }
+        if (A.Value.Item2 > B.Value.Item2)
+        {
+            return 1;
+        }
+        if (A.Value.Item1 < B.Value.Item1)
+        {
+            return -1;
+        }
+        if (A.Value.Item1 > B.Value.Item1)
+        {
+            return 1;
+        }
+        return 0;
     }
 }
