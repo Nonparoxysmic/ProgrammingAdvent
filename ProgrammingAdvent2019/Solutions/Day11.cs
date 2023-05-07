6b89c2c74554db1277d770552e7dbe3c2ae7a455
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-05-07 15:17:46 -0500

    Complete Day 11

--- ProgrammingAdvent2019/Solutions/Day11.cs
+++ ProgrammingAdvent2019/Solutions/Day11.cs
@@ -3,6 +3,7 @@
 // for Advent of Code 2019
 // https://adventofcode.com/2019
 
+using System.Drawing;
 using System.Text.RegularExpressions;
 using ProgrammingAdvent2019.Common;
 
@@ -50,7 +51,13 @@ internal class Day11 : Day
         {
             return output.WriteError(error);
         }
-        return output.WriteAnswers(robot.PanelsPainted, null);
+        int panelsPainted = robot.PanelsPainted;
+        robot = new(inputLines[0], 1);
+        if (!robot.Run(out string error2))
+        {
+            return output.WriteAnswers(panelsPainted, error2);
+        }
+        return output.WriteAnswers(panelsPainted, robot.IdentifierPainted);
     }
 
     private class Day11Robot
@@ -59,6 +66,7 @@ internal class Day11 : Day
         public int Y { get; private set; }
         public int Direction { get; private set; }
         public int PanelsPainted => _panelsPainted.Count;
+        public string IdentifierPainted { get; private set; } = string.Empty;
 
         private readonly Day09.Day09Program _program;
         private readonly HashSet<(int, int)> _whitePanels = new();
@@ -71,14 +79,18 @@ internal class Day11 : Day
             (0, 1)
         };
 
-        public Day11Robot(string intcode)
+        public Day11Robot(string intcode, int startingPanel = 0)
         {
             _program = new Day09.Day09Program(intcode);
+            if (startingPanel != 0)
+            {
+                _whitePanels.Add((X, Y));
+            }
         }
 
         public bool Run(out string error)
         {
-            Direction = 3;
+            Direction = 1;
             int timeout = 0;
             while (timeout++ < 250_000)
             {
@@ -95,6 +107,7 @@ internal class Day11 : Day
                         error = outputError;
                         return false;
                     }
+                    IdentifierPainted = ReadIdentifier(_whitePanels);
                     error = string.Empty;
                     return true;
                 }
@@ -144,17 +157,130 @@ internal class Day11 : Day
             _panelsPainted.Add((X, Y));
             if (turnDirection == 0)
             {
-                Direction++;
+                Direction += 3;
             }
             else
             {
-                Direction += 3;
+                Direction++;
             }
             Direction %= 4;
             X += steps[Direction].dX;
             Y += steps[Direction].dY;
             error = string.Empty;
             return true;
+        }
+
+        private static string ReadIdentifier(HashSet<(int, int)> whitePanels)
+        {
+            if (whitePanels is null || whitePanels.Count == 0)
+            {
+                return "ERROR: No white panels.";
+            }
+            Rectangle boundary = Boundary(whitePanels);
+            if (boundary.Height != 6 || boundary.Width < 38 || boundary.Width > 40)
+            {
+                return "ERROR: Painted panels are not the size of 8 letters.";
+            }
+            char[,] panels = new char[boundary.Width, boundary.Height];
+            for (int y = 0; y < boundary.Height; y++)
+            {
+                for (int x = 0; x < boundary.Width; x++)
+                {
+                    if (whitePanels.Contains((boundary.X + x, boundary.Y + y)))
+                    {
+                        panels[x, y] = '#';
+                    }
+                    else
+                    {
+                        panels[x, y] = '.';
+                    }
+                }
+            }
+            if (!TryReadLetters(panels, out string identifier))
+            {
+                return $"ERROR: Could not read all letters in identifier \"{identifier}\".";
+            }
+            return identifier;
+        }
+
+        private static Rectangle Boundary(HashSet<(int, int)> whitePanels)
+        {
+            int xMin = int.MaxValue, yMin = int.MaxValue;
+            int xMax = int.MinValue, yMax = int.MinValue;
+            foreach ((int panelX, int panelY) in whitePanels)
+            {
+                xMin = Math.Min(xMin, panelX);
+                yMin = Math.Min(yMin, panelY);
+                xMax = Math.Max(xMax, panelX);
+                yMax = Math.Max(yMax, panelY);
+            }
+            return new Rectangle(xMin, yMin, xMax - xMin + 1, yMax - yMin + 1);
+        }
+
+        public static bool TryReadLetters(char[,] points, out string result)
+        {
+            if (points.GetLength(1) != 6 || points.GetLength(0) < 3)
+            {
+                result = string.Empty;
+                return false;
+            }
+            int xPos = 1;
+            for (int y = 0; y < 6; y++)
+            {
+                if (points[0, y] == '#')
+                {
+                    xPos = 0;
+                    break;
+                }
+            }
+            List<char> parsedLetters = new();
+            if (FirstLetterIsI(points, xPos))
+            {
+                parsedLetters.Add('I');
+                xPos += 4;
+            }
+            bool foundUnknownLetters = false;
+            while (xPos < points.GetLength(0))
+            {
+                char[] bits = new char[30];
+                int i = 0;
+                for (int y = 0; y < 6; y++)
+                {
+                    for (int x = 0; x < 5; x++)
+                    {
+                        if (xPos + x >= points.GetLength(0))
+                        {
+                            bits[i++] = '0';
+                        }
+                        else
+                        {
+                            bits[i++] = points[xPos + x, y] == '#' ? '1' : '0';
+                        }
+                    }
+                }
+                if (!Day08.TryGetLetter(string.Join(null, bits), out char c))
+                {
+                    foundUnknownLetters = true;
+                }
+                parsedLetters.Add(c);
+                xPos += 5;
+            }
+            result = string.Join(null, parsedLetters);
+            return !foundUnknownLetters;
+        }
+
+        private static bool FirstLetterIsI(char[,] points, int x)
+        {
+            if (points.GetLength(0) < x + 3)
+            {
+                return false;
+            }
+            return points[x, 0] == '#' && points[x + 1, 0] == '#' && points[x + 2, 0] == '#'
+                && points[x, 1] == '.' && points[x + 1, 1] == '#' && points[x + 2, 1] == '.'
+                && points[x, 2] == '.' && points[x + 1, 2] == '#' && points[x + 2, 2] == '.'
+                && points[x, 3] == '.' && points[x + 1, 3] == '#' && points[x + 2, 3] == '.'
+                && points[x, 4] == '.' && points[x + 1, 4] == '#' && points[x + 2, 4] == '.'
+                && points[x, 5] == '#' && points[x + 1, 5] == '#' && points[x + 2, 5] == '#';
         }
     }
 }
