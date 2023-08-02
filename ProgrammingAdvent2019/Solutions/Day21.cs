4916dbc982391f6f2a8961b76db33f881319e24d
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-08-01 23:39:02 -0500

    Start Day 21

---
+++ ProgrammingAdvent2019/Solutions/Day21.cs
@@ -0,0 +1,33 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2019
+// https://adventofcode.com/2019
+
+using ProgrammingAdvent2019.Common;
+
+namespace ProgrammingAdvent2019.Solutions;
+
+internal class Day21 : Day
+{
+    public override bool ValidateInput(string[] inputLines, out string errorMessage)
+    {
+        if (inputLines.Length == 0 || inputLines[0].Length == 0)
+        {
+            errorMessage = "No input.";
+            return false;
+        }
+        if (!ValidateIntcodeInput(inputLines, out errorMessage))
+        {
+            return false;
+        }
+        errorMessage = string.Empty;
+        return true;
+    }
+
+    protected override PuzzleAnswers CalculateAnswers(string[] inputLines, string? exampleModifier = null)
+    {
+        PuzzleAnswers output = new();
+
+        return output.WriteAnswers(null, null);
+    }
+}
