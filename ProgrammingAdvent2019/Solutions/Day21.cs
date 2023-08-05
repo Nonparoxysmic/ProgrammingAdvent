0114fe4d82f8a6815bf7bc9de300f2f1b18e72c4
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-08-04 20:54:10 -0500

    Implement interactive testing program for Day 21

--- ProgrammingAdvent2019/Solutions/Day21.cs
+++ ProgrammingAdvent2019/Solutions/Day21.cs
@@ -30,4 +30,69 @@ internal class Day21 : Day
 
         return output.WriteAnswers(null, null);
     }
+
+    public class UserComputer
+    {
+        readonly Day09.Day09Program _program;
+
+        public UserComputer(string code)
+        {
+            _program = new(code);
+        }
+
+        public void Run(int timeout = 20)
+        {
+            Console.WriteLine("Running program...");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int i = 0;
+            while (++i < timeout)
+            {
+                while (_program.Tick()) { }
+                if (_program.Status == Day09.Day09Program.ProgramStatus.Error)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: " + _program.Error);
+                    Console.WriteLine();
+                    Console.ResetColor();
+                    return;
+                }
+                while (_program.OutputCount > 0)
+                {
+                    long output = _program.DequeueOutput();
+                    if (output == '\n' || (' ' <= output && output <= '~'))
+                    {
+                        Console.Write((char)output);
+                    }
+                    else
+                    {
+                        Console.Write(output);
+                    }
+                }
+                if (_program.Status == Day09.Day09Program.ProgramStatus.Waiting)
+                {
+                    string input = Console.ReadLine() ?? string.Empty;
+                    foreach (char c in input)
+                    {
+                        _program.EnqueueInput(c);
+                    }
+                    _program.EnqueueInput(10);
+                    continue;
+                }
+                if (_program.Status == Day09.Day09Program.ProgramStatus.Halted)
+                {
+                    break;
+                }
+            }
+            Console.ResetColor();
+            if (i >= timeout)
+            {
+                Console.WriteLine("Program ran too long and was timed out.");
+            }
+            else
+            {
+                Console.WriteLine("Program successfully halted.");
+            }
+            Console.WriteLine();
+        }
+    }
 }
