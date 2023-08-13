1a7ff5993f7606e0992dfb0693033ba73b9813de
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-08-12 23:50:19 -0500

    Start Day 22

---
+++ ProgrammingAdvent2019/Solutions/Day22.cs
@@ -0,0 +1,40 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2019
+// https://adventofcode.com/2019
+
+using System.Text.RegularExpressions;
+using ProgrammingAdvent2019.Common;
+
+namespace ProgrammingAdvent2019.Solutions;
+
+internal class Day22 : Day
+{
+    private static readonly Regex _validLine = new("^(deal into new stack|deal with increment (?<increment>[0-9]{1,2})|cut (?<cut>-?[0-9]{1,4}))$");
+
+    public override bool ValidateInput(string[] inputLines, out string errorMessage)
+    {
+        if (inputLines.Length == 0)
+        {
+            errorMessage = "No input.";
+            return false;
+        }
+        foreach (string line in inputLines)
+        {
+            if (!_validLine.IsMatch(line))
+            {
+                errorMessage = $"Invalid input line \"{line.Left(20, true)}\".";
+                return false;
+            }
+        }
+        errorMessage = string.Empty;
+        return true;
+    }
+
+    protected override PuzzleAnswers CalculateAnswers(string[] inputLines, string? exampleModifier = null)
+    {
+        PuzzleAnswers output = new();
+
+        return output.WriteAnswers(null, null);
+    }
+}
