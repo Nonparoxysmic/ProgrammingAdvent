7e4158503a1def6a5c38b7e2c4db8ab92ac83300
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-09-11 22:17:13 -0500

    Complete Day 8

--- ProgrammingAdvent2020/Solutions/Day08.cs
+++ ProgrammingAdvent2020/Solutions/Day08.cs
@@ -45,9 +45,113 @@ internal class Day08 : Day
         }
 
         int programCounter = 0, accumulator = 0;
-        HashSet<int> performedInstructions = new();
         while (0 <= programCounter && programCounter < instructions.Length)
         {
+            Instruction current = instructions[programCounter];
+            if (current.InTheLoop)
+            {
+                break;
+            }
+            current.InTheLoop = true;
+            if (current.Operation == 'a')
+            {
+                accumulator += current.Argument;
+            }
+            else if (current.Operation == 'j')
+            {
+                programCounter += current.Argument - 1;
+            }
+            programCounter++;
+        }
+        int partOneAnswer = accumulator;
+
+        List<int> possibleSwaps = SwapsToTry(instructions, out int? answer);
+        if (answer is not null)
+        {
+            possibleSwaps.Clear();
+            possibleSwaps.Add((int)answer);
+        }
+        if (possibleSwaps.Count == 0)
+        {
+            return output.WriteAnswers(partOneAnswer, "No answer found.");
+        }
+        int? partTwoAnswer = PartTwoAnswer(possibleSwaps, instructions);
+        if (partTwoAnswer is null)
+        {
+            return output.WriteAnswers(partOneAnswer, "No answer found.");
+        }
+
+        return output.WriteAnswers(partOneAnswer, partTwoAnswer);
+    }
+
+    private static List<int> SwapsToTry(Instruction[] instructions, out int? answer)
+    {
+        answer = null;
+        List<int> options = new();
+        bool jumpCouldSwap = true;
+        for (int i = instructions.Length - 1; i >= 0; i--)
+        {
+            Instruction current = instructions[i];
+            if (!current.InTheLoop)
+            {
+                jumpCouldSwap = true;
+                continue;
+            }
+            if (current.Operation == 'n')
+            {
+                int target = i + current.Argument;
+                if (target < 0 || target > instructions.Length)
+                {
+                    continue;
+                }
+                if (target == instructions.Length)
+                {
+                    answer = i;
+                    return options;
+                }
+                if (!instructions[target].InTheLoop)
+                {
+                    options.Add(i);
+                }
+            }
+            else if (current.Operation == 'j')
+            {
+                if (!jumpCouldSwap)
+                {
+                    continue;
+                }
+                jumpCouldSwap = false;
+                options.Add(i);
+            }
+        }
+        return options;
+    }
+
+    private static int? PartTwoAnswer(List<int> possibleSwaps, Instruction[] instructions)
+    {
+        foreach (int option in possibleSwaps)
+        {
+            instructions[option].Swap();
+            if (TryRunProgram(instructions, out int accumulator))
+            {
+                return accumulator;
+            }
+            instructions[option].Swap();
+        }
+        return null;
+    }
+
+    private static bool TryRunProgram(Instruction[] instructions, out int accumulator)
+    {
+        int programCounter = 0;
+        accumulator = 0;
+        HashSet<int> performedInstructions = new();
+        while (0 <= programCounter && programCounter <= instructions.Length)
+        {
+            if (programCounter == instructions.Length)
+            {
+                return true;
+            }
             if (!performedInstructions.Add(programCounter))
             {
                 break;
@@ -63,20 +167,31 @@ internal class Day08 : Day
             }
             programCounter++;
         }
-        int partOneAnswer = accumulator;
-
-        return output.WriteAnswers(partOneAnswer, null);
+        return false;
     }
 
     private class Instruction
     {
         public char Operation { get; private set; }
         public int Argument { get; private set; }
+        public bool InTheLoop { get; set; }
 
         public Instruction(char operation, int argument)
         {
             Operation = operation;
             Argument = argument;
+        }
+
+        public void Swap()
+        {
+            if (Operation == 'n')
+            {
+                Operation = 'j';
+            }
+            else if (Operation == 'j')
+            {
+                Operation = 'n';
+            }
         }
     }
 }
