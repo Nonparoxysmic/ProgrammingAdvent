a39459acff11c809af37ddf8f45f48b630f92f07
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2023-10-01 22:14:28 -0500

    Complete Day 17

--- ProgrammingAdvent2020/Solutions/Day17.cs
+++ ProgrammingAdvent2020/Solutions/Day17.cs
@@ -41,19 +41,22 @@ internal class Day17 : Day
         PuzzleAnswers output = new();
 
         char[,] initialSlice = input.ToCharArray2D();
-        Grid grid = new(initialSlice);
-        grid.SimulateCycles(6);
-        int partOneAnswer = grid.CountActiveCubes();
+        Grid3D grid3D = new(initialSlice);
+        grid3D.SimulateCycles(6);
+        int partOneAnswer = grid3D.CountActiveCubes();
+        Grid4D grid4D = new(initialSlice);
+        grid4D.SimulateCycles(6);
+        int partTwoAnswer = grid4D.CountActiveCubes();
 
-        return output.WriteAnswers(partOneAnswer, null);
+        return output.WriteAnswers(partOneAnswer, partTwoAnswer);
     }
 
-    private class Grid
+    private class Grid3D
     {
         private readonly Dictionary<(int, int, int), bool> _cubes = new();
         private readonly Dictionary<(int, int, int), int> _neighbors = new();
 
-        public Grid(char[,] initialSlice)
+        public Grid3D(char[,] initialSlice)
         {
             for (int y = 0; y < initialSlice.GetLength(1); y++)
             {
@@ -135,6 +138,119 @@ internal class Day17 : Day
             foreach (var kvp in _neighbors)
             {
                 (int, int, int) position = kvp.Key;
+                int neighbors = kvp.Value;
+                if (!_cubes.ContainsKey(position))
+                {
+                    _cubes.Add(position, false);
+                }
+                if (_cubes[position])
+                {
+                    if (neighbors < 2 || neighbors > 3)
+                    {
+                        _cubes[position] = false;
+                    }
+                }
+                else
+                {
+                    if (neighbors == 3)
+                    {
+                        _cubes[position] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private class Grid4D
+    {
+        private readonly Dictionary<(int, int, int, int), bool> _cubes = new();
+        private readonly Dictionary<(int, int, int, int), int> _neighbors = new();
+
+        public Grid4D(char[,] initialSlice)
+        {
+            for (int y = 0; y < initialSlice.GetLength(1); y++)
+            {
+                for (int x = 0; x < initialSlice.GetLength(0); x++)
+                {
+                    _cubes.Add((x, y, 0, 0), initialSlice[x, y] == '#');
+                }
+            }
+        }
+
+        public void SimulateCycles(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                ExecuteCycle();
+            }
+        }
+
+        public int CountActiveCubes()
+        {
+            return _cubes.Values.Count(isActive => isActive);
+        }
+
+        private void ExecuteCycle()
+        {
+            ClearNeighbors();
+            CountNeighbors();
+            UpdateActive();
+        }
+
+        private void ClearNeighbors()
+        {
+            foreach (var key in _neighbors.Keys)
+            {
+                _neighbors[key] = 0;
+            }
+        }
+
+        private void CountNeighbors()
+        {
+            foreach (var kvp in _cubes)
+            {
+                if (kvp.Value)
+                {
+                    AddNeighbors(kvp.Key);
+                }
+            }
+        }
+
+        private void AddNeighbors((int X, int Y, int Z, int W) cube)
+        {
+            for (int Δz = -1; Δz <= 1; Δz++)
+            {
+                for (int Δy = -1; Δy <= 1; Δy++)
+                {
+                    for (int Δx = -1; Δx <= 1; Δx++)
+                    {
+                        for (int Δw = -1; Δw <= 1; Δw++)
+                        {
+                            if (Δx == 0 && Δy == 0 && Δz == 0 && Δw == 0) { continue; }
+                            AddNeighbor((cube.X + Δx, cube.Y + Δy, cube.Z + Δz, cube.W + Δw));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddNeighbor((int, int, int, int) position)
+        {
+            if (!_neighbors.ContainsKey(position))
+            {
+                _neighbors.Add(position, 1);
+            }
+            else
+            {
+                _neighbors[position]++;
+            }
+        }
+
+        private void UpdateActive()
+        {
+            foreach (var kvp in _neighbors)
+            {
+                (int, int, int, int) position = kvp.Key;
                 int neighbors = kvp.Value;
                 if (!_cubes.ContainsKey(position))
                 {
