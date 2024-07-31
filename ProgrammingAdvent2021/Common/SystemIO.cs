acc297073d344f830ee63d7463a721b0ada315a4
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-07-30 22:39:58 -0500

    Add input files

---
+++ ProgrammingAdvent2021/Common/SystemIO.cs
@@ -0,0 +1,29 @@
+﻿// ProgrammingAdvent2021 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Common;
+
+internal static class SystemIO
+{
+    public static bool TryReadInputFile(int dayNumber, out string[] lines)
+    {
+        string path = $"InputFiles\\InputDay{dayNumber:00}.txt";
+        if (dayNumber < 1 || dayNumber > 25 || !File.Exists(path))
+        {
+            lines = [];
+            return false;
+        }
+        try
+        {
+            lines = File.ReadAllLines(path);
+            return true;
+        }
+        catch
+        {
+            lines = [];
+            return false;
+        }
+    }
+}
