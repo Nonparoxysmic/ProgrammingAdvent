5263bc36b7a8b3908d7960ce4327dad508d92229
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-01 22:21:10 -0500

    Add solution class

---
+++ ProgrammingAdvent2021/Solutions/Day.cs
@@ -0,0 +1,29 @@
+﻿// ProgrammingAdvent2021 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+using System.Diagnostics;
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal abstract class Day
+{
+    public void Solve(string[] input)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        (string, string) answers = CalculateAnswers(input);
+        stopwatch.Stop();
+
+        int dayNumber = int.Parse(GetType().ToString()[^2..^0]);
+        // TODO: Print results better
+        Console.WriteLine($"Day {dayNumber}: {answers} {stopwatch.ElapsedMilliseconds}");
+    }
+
+    public (string, string) Test(string[] input)
+    {
+        return CalculateAnswers(input);
+    }
+
+    protected abstract (string, string) CalculateAnswers(string[] input);
+}
---
+++ ProgrammingAdvent2021/Solutions/DayTemplate.cs
@@ -0,0 +1,14 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal class DayTemplate : Day
+{
+    protected override (string, string) CalculateAnswers(string[] input)
+    {
+        return ("Solution not yet implemented", "n/a");
+    }
+}
