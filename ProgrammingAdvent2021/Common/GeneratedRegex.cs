429a1077054557578c0b3be5d371decb02d30f95
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-02 20:51:05 -0500

    Add class for generated Regex

---
+++ ProgrammingAdvent2021/Common/GeneratedRegex.cs
@@ -0,0 +1,16 @@
+﻿// ProgrammingAdvent2021 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+using System.Text.RegularExpressions;
+
+namespace ProgrammingAdvent2021.Common;
+
+internal static partial class GeneratedRegex
+{
+    public static readonly Regex ValidDayName = ValidDayNameRegex();
+
+    [GeneratedRegex("^Day(?<DayNumber>0[1-9]|1[0-9]|2[0-5])$")]
+    private static partial Regex ValidDayNameRegex();
+}
