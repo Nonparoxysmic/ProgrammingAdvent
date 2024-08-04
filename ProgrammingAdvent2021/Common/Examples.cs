546b4d09c30efb1b1ea74c878723051708f9b23d
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-03 21:50:13 -0500

    Read examples from JSON file

---
+++ ProgrammingAdvent2021/Common/Examples.cs
@@ -0,0 +1,70 @@
+﻿// ProgrammingAdvent2021 by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+using System.Text.Json;
+
+namespace ProgrammingAdvent2021.Common;
+
+internal static class Examples
+{
+    private static readonly SortedDictionary<int, (string, string?, string?)[]> _examples = [];
+
+    public static void Initialize()
+    {
+        _examples.Clear();
+        if (!SystemIO.TryReadExampleFile(out JsonDocument? jsonDocument))
+        {
+            return;
+        }
+        for (int dayNumber = 1; dayNumber <= 25; dayNumber++)
+        {
+            if (jsonDocument.RootElement.TryGetProperty($"Day{dayNumber:00}", out JsonElement dayProperty))
+            {
+                List<(string, string?, string?)> dayExamples = [];
+                for (int i = 1; i < 100; i++)
+                {
+                    if (!dayProperty.TryGetProperty($"Example{i:00}", out JsonElement exampleProperty))
+                    {
+                        break;
+                    }
+                    if (!exampleProperty.TryGetProperty("Input", out JsonElement inputProperty)
+                        || inputProperty.GetString() is null)
+                    {
+                        break;
+                    }
+                    string input = inputProperty.GetString() ?? string.Empty;
+                    string? partOne = null, partTwo = null;
+                    if (exampleProperty.TryGetProperty("PartOne", out JsonElement partOneProperty))
+                    {
+                        partOne = partOneProperty.GetString();
+                    }
+                    if (exampleProperty.TryGetProperty("PartTwo", out JsonElement partTwoProperty))
+                    {
+                        partTwo = partTwoProperty.GetString();
+                    }
+                    if (partOne is not null || partTwo is not null)
+                    {
+                        dayExamples.Add((input, partOne, partTwo));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (dayExamples.Count > 0)
+                {
+                    _examples.Add(dayNumber, [.. dayExamples]);
+                }
+            }
+        }
+        jsonDocument.Dispose();
+    }
+
+    public static void TestExamples()
+    {
+        // TODO: Test examples
+        Console.WriteLine("Example testing not yet implemented.");
+    }
+}
--- ProgrammingAdvent2021/Program.cs
+++ ProgrammingAdvent2021/Program.cs
@@ -2,3 +2,22 @@
 // https://github.com/Nonparoxysmic/ProgrammingAdvent
 // for Advent of Code 2021
 // https://adventofcode.com/2021
+
+using ProgrammingAdvent2021.Common;
+using ProgrammingAdvent2021.Utilities;
+
+Console.Title = "ProgrammingAdvent2021 by Nonparoxysmic";
+
+Console.Write("Press E to test examples, any other key to solve");
+char key = (char)Console.ReadKey(true).Key;
+Console.Clear();
+if (key.ToUpper() == 'E')
+{
+    Examples.Initialize();
+    Examples.TestExamples();
+}
+else
+{
+    // TODO: Solve puzzles
+    Console.WriteLine("Solving not yet implemented.");
+}
