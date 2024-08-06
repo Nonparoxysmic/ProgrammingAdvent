00fd5d16b1551edeb77d008e2db4a82b3534a222
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-05 20:10:32 -0500

    Complete Day 1 Part One

---
+++ ProgrammingAdvent2021/Solutions/Day01.cs
@@ -0,0 +1,25 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal class Day01 : Day
+{
+    protected override (string, string) CalculateAnswers(string[] input)
+    {
+        IEnumerable<int> values = input.Select(str => int.Parse(str));
+        int previous = int.MaxValue;
+        int partOneAnswer = 0;
+        foreach (int value in values)
+        {
+            if (value > previous)
+            {
+                partOneAnswer++;
+            }
+            previous = value;
+        }
+        return ($"{partOneAnswer}", "n/a");
+    }
+}
