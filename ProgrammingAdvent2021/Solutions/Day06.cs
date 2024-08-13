07f6ed6ebaf833d71385dfeb2fac2f1c4cdb4e36
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-12 22:43:12 -0500

    Complete Day 6 Part One

---
+++ ProgrammingAdvent2021/Solutions/Day06.cs
@@ -0,0 +1,31 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal class Day06 : Day
+{
+    protected override (string, string) CalculateAnswers(string[] input)
+    {
+        List<int> fish = input[0].Split(',').Select(str => int.Parse(str)).ToList();
+        for (int day = 0; day < 80; day++)
+        {
+            int len = fish.Count;
+            for (int i = 0; i < len; i++)
+            {
+                if (fish[i] == 0)
+                {
+                    fish[i] = 6;
+                    fish.Add(8);
+                }
+                else
+                {
+                    fish[i]--;
+                }
+            }
+        }
+        return ($"{fish.Count}", "n/a");
+    }
+}
