cc90ddb24a316af4a442ce2a529cc384c273135f
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-14 21:47:34 -0500

    Complete Day 7 Part One

---
+++ ProgrammingAdvent2021/Solutions/Day07.cs
@@ -0,0 +1,22 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal class Day07 : Day
+{
+    protected override (string, string) CalculateAnswers(string[] input)
+    {
+        int[] initialPositions = input[0].Split(',').Select(str => int.Parse(str)).ToArray();
+        Array.Sort(initialPositions);
+        int len = initialPositions.Length;
+        int partOnePosition = len % 2 == 1
+            ? initialPositions[len / 2]
+            : (initialPositions[len / 2] + initialPositions[len / 2 - 1]) / 2;
+        int partOneAnswer = initialPositions
+            .Aggregate(0, (total, next) => total + Math.Abs(partOnePosition - next));
+        return ($"{partOneAnswer}", "n/a");
+    }
+}
