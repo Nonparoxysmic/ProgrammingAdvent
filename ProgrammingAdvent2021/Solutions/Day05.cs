61b018a2ac744ec83f8428cace85668b58e6262e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-17 22:31:36 -0500

    Complete Day 5

--- ProgrammingAdvent2021/Solutions/Day05.cs
+++ ProgrammingAdvent2021/Solutions/Day05.cs
@@ -12,6 +12,13 @@ internal class Day05 : Day
 {
     protected override (string, string) CalculateAnswers(string[] input)
     {
+        return SimpleMethod(input);
+    }
+
+    [System.Diagnostics.CodeAnalysis
+        .SuppressMessage("CodeQuality", "IDE0051:Remove unused private members")]
+    private static (string, string) ComplicatedMethod(string[] input) // DOESN'T WORK
+    {
         List<VentLine> horizontalLines = [];
         List<VentLine> verticalLines = [];
         List<VentLine> diagonalLines = [];
@@ -111,11 +118,35 @@ internal class Day05 : Day
             {
                 if (diagonalLines[i].DiagonalUp && diagonalLines[j].DiagonalUp)
                 {
-                    // TODO: Check for overlap
+                    // x - y = C
+                    if (diagonalLines[i].X1 - diagonalLines[i].Y1
+                        == diagonalLines[j].X1 - diagonalLines[j].Y1)
+                    {
+                        // Lines are collinear
+                        int lower = Math.Max(diagonalLines[i].X1, diagonalLines[j].X1);
+                        int upper = Math.Min(diagonalLines[i].X2, diagonalLines[j].X2);
+                        int C = diagonalLines[i].X1 - diagonalLines[i].Y1;
+                        for (int x = lower; x <= upper; x++)
+                        {
+                            intersections.Add((x, x - C));
+                        }
+                    }
                 }
                 else if (!diagonalLines[i].DiagonalUp && !diagonalLines[j].DiagonalUp)
                 {
-                    // TODO: Check for overlap
+                    // x + y = C
+                    if (diagonalLines[i].X1 + diagonalLines[i].Y1
+                        == diagonalLines[j].X1 + diagonalLines[j].Y1)
+                    {
+                        // Lines are collinear
+                        int lower = Math.Max(diagonalLines[i].X1, diagonalLines[j].X1);
+                        int upper = Math.Min(diagonalLines[i].X2, diagonalLines[j].X2);
+                        int C = diagonalLines[i].X1 + diagonalLines[i].Y1;
+                        for (int x = lower; x <= upper; x++)
+                        {
+                            intersections.Add((x, C - x));
+                        }
+                    }
                 }
                 else if (LinesIntersect(diagonalLines[i], diagonalLines[j]))
                 {
@@ -124,6 +155,87 @@ internal class Day05 : Day
             }
         }
         int partTwoAnswer = intersections.Count;
+
+        return ($"{partOneAnswer}", $"{partTwoAnswer}");
+    }
+
+    private static (string, string) SimpleMethod(string[] input)
+    {
+        List<VentLine> orthogonalLines = [];
+        List<VentLine> diagonalLines = [];
+        foreach (string line in input)
+        {
+            Match match = GeneratedRegex.ValidDay05InputLine.Match(line);
+            int x1 = int.Parse(match.Groups["x1"].Value);
+            int y1 = int.Parse(match.Groups["y1"].Value);
+            int x2 = int.Parse(match.Groups["x2"].Value);
+            int y2 = int.Parse(match.Groups["y2"].Value);
+            if (y1 == y2 || x1 == x2)
+            {
+                orthogonalLines.Add(new VentLine(x1, y1, x2, y2));
+            }
+            else
+            {
+                diagonalLines.Add(new VentLine(x1, y1, x2, y2));
+            }
+        }
+
+        // PART ONE
+        int[,] lines = new int[1000, 1000];
+        foreach (var line in orthogonalLines)
+        {
+            (int, int) step = (Math.Sign(line.X2 - line.X1), Math.Sign(line.Y2 - line.Y1));
+            for ((int x, int y) = (line.X1, line.Y1); ;)
+            {
+                lines[x, y]++;
+                if ((x, y) == (line.X2, line.Y2))
+                {
+                    break;
+                }
+                x += step.Item1;
+                y += step.Item2;
+            }
+        }
+        int overlaps = 0;
+        for (int y = 0; y < lines.GetLength(1); y++)
+        {
+            for (int x = 0; x < lines.GetLength(0); x++)
+            {
+                if (lines[x, y] > 1)
+                {
+                    overlaps++;
+                }
+            }
+        }
+        int partOneAnswer = overlaps;
+
+        // PART TWO
+        foreach (var line in diagonalLines)
+        {
+            (int, int) step = (Math.Sign(line.X2 - line.X1), Math.Sign(line.Y2 - line.Y1));
+            for ((int x, int y) = (line.X1, line.Y1); ;)
+            {
+                lines[x, y]++;
+                if ((x, y) == (line.X2, line.Y2))
+                {
+                    break;
+                }
+                x += step.Item1;
+                y += step.Item2;
+            }
+        }
+        overlaps = 0;
+        for (int y = 0; y < lines.GetLength(1); y++)
+        {
+            for (int x = 0; x < lines.GetLength(0); x++)
+            {
+                if (lines[x, y] > 1)
+                {
+                    overlaps++;
+                }
+            }
+        }
+        int partTwoAnswer = overlaps;
 
         return ($"{partOneAnswer}", $"{partTwoAnswer}");
     }
@@ -155,10 +267,10 @@ internal class Day05 : Day
         int s2_x = p3_x - p2_x;
         int s2_y = p3_y - p2_y;
 
-        int t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+        float t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (float)(-s2_x * s1_y + s1_x * s2_y);
 
-        int i_x = p0_x + (t * s1_x);
-        int i_y = p0_y + (t * s1_y);
+        int i_x = p0_x + (int)Math.Round(t * s1_x);
+        int i_y = p0_y + (int)Math.Round(t * s1_y);
         return (i_x, i_y);
     }
 
