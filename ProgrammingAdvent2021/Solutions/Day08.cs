21f419da0fd72fb63e1defadb2f432ddaac3b568
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-08-20 20:57:55 -0500

    Complete Day 8 Part One

---
+++ ProgrammingAdvent2021/Solutions/Day08.cs
@@ -0,0 +1,30 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2021
+// https://adventofcode.com/2021
+
+namespace ProgrammingAdvent2021.Solutions;
+
+internal class Day08 : Day
+{
+    protected override (string, string) CalculateAnswers(string[] input)
+    {
+        int[] outputLengths = new int[8];
+        foreach (string line in input)
+        {
+            string[] terms = line.Split(' ');
+            if (terms.Length != 15)
+            {
+                return ("Invalid line in input.", "n/a");
+            }
+            for (int i = 11; i < 15; i++)
+            {
+                outputLengths[terms[i].Length]++;
+            }
+        }
+        int partOneAnswer = outputLengths[2] + outputLengths[3]
+            + outputLengths[4] + outputLengths[7];
+
+        return ($"{partOneAnswer}", "n/a");
+    }
+}
