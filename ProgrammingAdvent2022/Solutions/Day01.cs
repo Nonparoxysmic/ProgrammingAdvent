f1e7009f705ed4e482b92830ec998ad8db3d405c
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-10-11 22:30:43 -0500

    Complete Day 1 Part One

---
+++ ProgrammingAdvent2022/Solutions/Day01.cs
@@ -0,0 +1,41 @@
+﻿// Puzzle solution by Nonparoxysmic
+// https://github.com/Nonparoxysmic/ProgrammingAdvent
+// for Advent of Code 2022
+// https://adventofcode.com/2022
+
+using ProgrammingAdvent2022.Common;
+
+namespace ProgrammingAdvent2022.Solutions;
+
+internal class Day01 : Day
+{
+    protected override PuzzleAnswers CalculateAnswers(string[] input, string? exampleModifier = null)
+    {
+        PuzzleAnswers result = new();
+
+        bool validInput = input.Aggregate(true, (valid, next) =>
+            valid && (next == string.Empty || int.TryParse(next, out int _)));
+        if (!validInput)
+        {
+            return result.WriteError("Invalid input.");
+        }
+
+        List<int> calorieTotals = [];
+        int sum = 0;
+        foreach (string line in input)
+        {
+            if (line == string.Empty)
+            {
+                calorieTotals.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += int.Parse(line);
+            }
+        }
+        int mostTotalCalories = calorieTotals.Max();
+
+        return result.WriteAnswers(mostTotalCalories, null);
+    }
+}
