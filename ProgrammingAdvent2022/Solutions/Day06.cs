1b12c3e9f9f1aa41fdf90c36164a198f040d5d94
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-10-17 22:05:55 -0500

    Complete Day 6

--- ProgrammingAdvent2022/Solutions/Day06.cs
+++ ProgrammingAdvent2022/Solutions/Day06.cs
@@ -14,8 +14,9 @@ internal class Day06 : Day
         PuzzleAnswers result = new();
 
         int indexOfStartOfPacket = IndexOfStartOfPacket(input[0]);
+        int indexOfStartOfMessage = IndexOfStartOfMessage(input[0]);
 
-        return result.WriteAnswers(indexOfStartOfPacket, null);
+        return result.WriteAnswers(indexOfStartOfPacket, indexOfStartOfMessage);
     }
 
     private static int IndexOfStartOfPacket(string signal)
@@ -28,6 +29,33 @@ internal class Day06 : Day
             {
                 return i + 1;
             }
+        }
+        return -1;
+    }
+
+    private static int IndexOfStartOfMessage(string signal)
+    {
+        if (signal.Length < 14)
+        {
+            return -1;
+        }
+        int[] charCounts = new int[26];
+        for (int i = 0; i < 14; i++)
+        {
+            charCounts[signal[i] - 'a']++;
+        }
+        for (int i = 14; i <= signal.Length; i++)
+        {
+            if (charCounts.Max() == 1)
+            {
+                return i;
+            }
+            if (i == signal.Length)
+            {
+                break;
+            }
+            charCounts[signal[i - 14] - 'a']--;
+            charCounts[signal[i] - 'a']++;
         }
         return -1;
     }
