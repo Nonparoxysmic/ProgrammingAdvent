9d448fa0b2ba775dae06c5ffb4dab085adeb3b00
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-10-26 22:08:49 -0500

    Complete Day 10

--- ProgrammingAdvent2022/Solutions/Day10.cs
+++ ProgrammingAdvent2022/Solutions/Day10.cs
@@ -3,6 +3,7 @@
 // for Advent of Code 2022
 // https://adventofcode.com/2022
 
+using System.Text;
 using ProgrammingAdvent2022.Common;
 
 namespace ProgrammingAdvent2022.Solutions;
@@ -16,8 +17,9 @@ internal class Day10 : Day
         int[] signal = InputToSignal(input);
         int partOneAnswer = 20 * signal[20] + 60 * signal[60] + 100 * signal[100]
             + 140 * signal[140] + 180 * signal[180] + 220 * signal[220];
+        char[] lettersOnScreen = ProcessSignal(signal);
 
-        return result.WriteAnswers(partOneAnswer, null);
+        return result.WriteAnswers(partOneAnswer, lettersOnScreen);
     }
 
     private static int[] InputToSignal(string[] input)
@@ -39,5 +41,58 @@ internal class Day10 : Day
             }
         }
         return signal;
+    }
+
+    private static char[] ProcessSignal(int[] signal)
+    {
+        char[] letters = new char[8];
+        StringBuilder sb = new();
+        for (int i = 0; i < 8; i++)
+        {
+            sb.Clear();
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = i * 5; x < i * 5 + 5; x++)
+                {
+                    int cycle = 40 * y + x + 1;
+                    if (Math.Abs(x - signal[cycle]) > 1)
+                    {
+                        sb.Append('.');
+                    }
+                    else
+                    {
+                        sb.Append('#');
+                    }
+                }
+            }
+            letters[i] = ReadLetter(sb.ToString());
+        }
+        return letters;
+    }
+
+    public static char ReadLetter(string pixels)
+    {
+        return pixels switch
+        {
+            ".##..#..#.#..#.####.#..#.#..#." => 'A',
+            "###..#..#.###..#..#.#..#.###.." => 'B',
+            ".##..#..#.#....#....#..#..##.." => 'C',
+            "####.#....###..#....#....####." => 'E',
+            "####.#....###..#....#....#...." => 'F',
+            ".##..#..#.#....#.##.#..#..###." => 'G',
+            "#..#.#..#.####.#..#.#..#.#..#." => 'H',
+            ".###...#....#....#....#...###." => 'I',
+            "..##....#....#....#.#..#..##.." => 'J',
+            "#..#.#.#..##...#.#..#.#..#..#." => 'K',
+            "#....#....#....#....#....####." => 'L',
+            ".##..#..#.#..#.#..#.#..#..##.." => 'O',
+            "###..#..#.#..#.###..#....#...." => 'P',
+            "###..#..#.#..#.###..#.#..#..#." => 'R',
+            ".###.#....#.....##.....#.###.." => 'S',
+            "#..#.#..#.#..#.#..#.#..#..##.." => 'U',
+            "#...##...#.#.#...#....#....#.." => 'Y',
+            "####....#...#...#...#....####." => 'Z',
+            _ => '?'
+        };
     }
 }
