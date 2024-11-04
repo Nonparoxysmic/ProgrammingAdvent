b88c5b11a0f7de394f54628f8c5bd9be73cbdad8
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-11-03 21:05:22 -0600

    Complete Day 15

--- ProgrammingAdvent2022/Solutions/Day15.cs
+++ ProgrammingAdvent2022/Solutions/Day15.cs
@@ -22,8 +22,10 @@ internal partial class Day15 : Day
         int row = exampleModifier is null ? 2_000_000 : 10;
         List<Sensor> sensors = ParseInput(input);
         int singleRowPositionsWithoutBeacons = PositionsWithoutBeacons(sensors, row);
+        Vector2Int distressBeaconPosition = DistressBeaconPosition(sensors, row * 2);
+        long tuningFrequency = distressBeaconPosition.X * 4_000_000L + distressBeaconPosition.Y;
 
-        return result.WriteAnswers(singleRowPositionsWithoutBeacons, null);
+        return result.WriteAnswers(singleRowPositionsWithoutBeacons, tuningFrequency);
     }
 
     private static List<Sensor> ParseInput(string[] input)
@@ -92,6 +94,142 @@ internal partial class Day15 : Day
         return count;
     }
 
+    private static Vector2Int DistressBeaconPosition(List<Sensor> sensors, int coordLimit)
+    {
+        if (TrySensorPairs(sensors, coordLimit, out Vector2Int answer))
+        {
+            return answer;
+        }
+        //for (int y = coordLimit; y >= 0; y--)
+        //{
+        //    int x = FirstPossiblePosition(sensors, y);
+        //    if (0 <= x && x <= coordLimit)
+        //    {
+        //        return new Vector2Int(x, y);
+        //    }
+        //}
+        return Vector2Int.Zero;
+    }
+
+    private static bool TrySensorPairs(List<Sensor> sensors, int coordLimit, out Vector2Int answer)
+    {
+        List<(int, int, int)> pairIndices = [];
+        for (int i = 0; i < sensors.Count - 1; i++)
+        {
+            for (int j = i + 1; j < sensors.Count; j++)
+            {
+                int distance = (sensors[i].Position - sensors[j].Position).TaxicabMagnitude();
+                int targetDistance = sensors[i].Radius + sensors[j].Radius + 2;
+                pairIndices.Add((distance - targetDistance, i, j));
+            }
+        }
+        List<(int, int, int)> matches = pairIndices.Where(t => t.Item1 == 0).ToList();
+        if (matches.Count > 1)
+        {
+            for (int i = 0; i <= matches.Count - 1; i++)
+            {
+                for (int j = i + 1; j < matches.Count; j++)
+                {
+                    if (HasValidY(matches[i], matches[j], sensors, out int y))
+                    {
+                        for (int dy = 0; dy < coordLimit; dy++)
+                        {
+                            if (0 <= y + dy && y + dy <= coordLimit)
+                            {
+                                int x = FirstPossiblePosition(sensors, y + dy);
+                                if (0 <= x && x <= coordLimit)
+                                {
+                                    answer = new Vector2Int(x, y + dy);
+                                    return true;
+                                }
+                            }
+                            if (0 <= y - dy && y - dy <= coordLimit)
+                            {
+                                int x = FirstPossiblePosition(sensors, y - dy);
+                                if (0 <= x && x <= coordLimit)
+                                {
+                                    answer = new Vector2Int(x, y - dy);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        answer = Vector2Int.Zero;
+        return false;
+    }
+
+    private static bool HasValidY((int, int, int) match1, (int, int, int) match2,
+        List<Sensor> sensors, out int y)
+    {
+        y = 0;
+        Sensor sensor0 = sensors[match1.Item2];
+        Sensor sensor1 = sensors[match1.Item3];
+        Sensor sensor2 = sensors[match2.Item2];
+        Sensor sensor3 = sensors[match2.Item3];
+        if (sensor0 == sensor2 || sensor0 == sensor3 || sensor1 == sensor2 || sensor1 == sensor3)
+        {
+            return false;
+        }
+        if (sensor0.Position.Y == sensor1.Position.Y)
+        {
+            y = sensor0.Position.Y;
+            return true;
+        }
+        if (sensor2.Position.Y == sensor3.Position.Y)
+        {
+            y = sensor2.Position.Y;
+            return true;
+        }
+        if (sensor0.Position.X == sensor1.Position.X)
+        {
+            Sensor lowest = sensor0.Position.Y < sensor1.Position.Y ? sensor0 : sensor1;
+            y = lowest.Position.Y + lowest.Radius + 1;
+            return true;
+        }
+        if (sensor2.Position.X == sensor3.Position.X)
+        {
+            Sensor lowest = sensor2.Position.Y < sensor3.Position.Y ? sensor2 : sensor3;
+            y = lowest.Position.Y + lowest.Radius + 1;
+            return true;
+        }
+
+        y = MoreMath.Average(sensor0.Position.Y, sensor1.Position.Y, sensor2.Position.Y, sensor3.Position.Y);
+        return true;
+    }
+
+    private static int FirstPossiblePosition(List<Sensor> sensors, int row)
+    {
+        List<(int, int)> exclusionZones = [];
+        foreach (Sensor sensor in sensors)
+        {
+            if (sensor.HasExclusionZone(row, out int xMin, out int xMax))
+            {
+                exclusionZones.Add((xMin, xMax));
+            }
+        }
+        List<(int, int)> activeRanges = [];
+        foreach ((int, int) range in exclusionZones)
+        {
+            List<(int, int)> touchingRanges = activeRanges.Where(r => RangesTouch(r, range)).ToList();
+            if (touchingRanges.Count == 0)
+            {
+                activeRanges.Add(range);
+                continue;
+            }
+            (int, int) merged = range;
+            foreach ((int, int) connection in touchingRanges)
+            {
+                merged = MergeRanges(merged, connection);
+                activeRanges.Remove(connection);
+            }
+            activeRanges.Add(merged);
+        }
+        return activeRanges[0].Item2 + 1;
+    }
+
     private static bool RangesIntersect((int xMin, int xMax) a, (int xMin, int xMax) b)
     {
         return (a.xMin <= b.xMin && b.xMin <= a.xMax)
@@ -111,6 +249,20 @@ internal partial class Day15 : Day
             yield return (range.xMin, exclude.xMin - 1);
         if (exclude.xMax < range.xMax)
             yield return(exclude.xMax + 1, range.xMax);
+    }
+
+    private static bool RangesTouch((int xMin, int xMax) a, (int xMin, int xMax) b)
+    {
+        if (RangesIntersect(a, b))
+        {
+            return true;
+        }
+        return a.xMax + 1 == b.xMin || b.xMax + 1 == a.xMin;
+    }
+
+    private static (int, int) MergeRanges((int xMin, int xMax) a, (int xMin, int xMax) b)
+    {
+        return (Math.Min(a.xMin, b.xMin), Math.Max(a.xMax, b.xMax));
     }
 
     private class Sensor
