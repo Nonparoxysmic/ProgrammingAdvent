9656780943fc1f80a7e7d6a0fcc331a609c6bb1b
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2024-12-10 21:13:17 -0600

    Complete Day 22

--- ProgrammingAdvent2022/Solutions/Day22.cs
+++ ProgrammingAdvent2022/Solutions/Day22.cs
@@ -3,6 +3,7 @@
 // for Advent of Code 2022
 // https://adventofcode.com/2022
 
+using System.Text;
 using ProgrammingAdvent2022.Common;
 
 namespace ProgrammingAdvent2022.Solutions;
@@ -17,11 +18,14 @@ internal class Day22 : Day
         {
             return result.WriteError("Could not parse input.");
         }
+
         Tile start = StartTile(sections);
         BuildGraph(sections);
         int password = FollowPath(path, start);
+        BuildCube(sections);
+        int actualPassword = FollowPath(path, start);
 
-        return result.WriteAnswers(password, null);
+        return result.WriteAnswers(password, actualPassword);
     }
 
     private static bool TryParseInput(string[] input, out Section?[,] sections, out string path)
@@ -152,7 +156,7 @@ internal class Day22 : Day
         }
     }
 
-    private static int FollowPath(string path, Tile start)
+    private static int FollowPath(string path, Tile start, bool ignoreWalls = false)
     {
         Tile current = start;
         int facing = 0;
@@ -186,17 +190,174 @@ internal class Day22 : Day
                 }
                 for (int j = 0; j < steps; j++)
                 {
-                    if (current.Neighbors[facing].Contents == '#')
+                    if (current.Neighbors[facing].Contents == '#' && !ignoreWalls)
                     {
                         break;
                     }
+                    int newFacing = (facing + current.NeighborRotation[facing]) % 4;
                     current = current.Neighbors[facing];
+                    facing = newFacing;
                 }
                 continue;
             }
             break;
         }
         return 1000 * current.Row + 4 * current.Column + facing;
+    }
+
+    private static void BuildCube(Section?[,] sectionArray)
+    {
+        DisconnectEdges(sectionArray);
+        IEnumerable<Section> sections = sectionArray.Cast<Section>().Where(s => s is not null);
+        bool allConnected = false;
+        while (!allConnected)
+        {
+            allConnected = true;
+            foreach (Section section in sections)
+            {
+                Tile topLeft = section.Tiles[0, 0];
+                if (HasAdjacentSections(topLeft, 2))
+                {
+                    ConnectCorner(topLeft, 2, section.Size);
+                }
+                else
+                {
+                    allConnected = false;
+                }
+
+                Tile topRight = section.Tiles[section.Size - 1, 0];
+                if (HasAdjacentSections(topRight, 3))
+                {
+                    ConnectCorner(topRight, 3, section.Size);
+                }
+                else
+                {
+                    allConnected = false;
+                }
+
+                Tile bottomLeft = section.Tiles[0, section.Size - 1];
+                if (HasAdjacentSections(bottomLeft, 1))
+                {
+                    ConnectCorner(bottomLeft, 1, section.Size);
+                }
+                else
+                {
+                    allConnected = false;
+                }
+
+                Tile bottomRight = section.Tiles[section.Size - 1, section.Size - 1];
+                if (HasAdjacentSections(bottomRight, 0))
+                {
+                    ConnectCorner(bottomRight, 0, section.Size);
+                }
+                else
+                {
+                    allConnected = false;
+                }
+            }
+        }
+    }
+
+    private static bool HasAdjacentSections(Tile corner, int direction)
+    {
+        return corner.Neighbors[direction] != corner && corner.Neighbors[(direction + 1) % 4] != corner;
+    }
+
+    private static void ConnectCorner(Tile corner, int direction1, int size)
+    {
+        if (CornerIsConnected(corner, direction1))
+        {
+            return;
+        }
+        int direction2 = (direction1 + 1) % 4;
+        Tile left = corner.Neighbors[direction1];
+        Tile right = corner.Neighbors[direction2];
+        direction1 = UpdateFacing(corner, direction1);
+        direction2 = UpdateFacing(corner, direction2);
+        int neighbor1 = (direction1 + 1) % 4;
+        int neighbor2 = (direction2 + 3) % 4;
+        for (int i = 0; i < size; i++)
+        {
+            left.Neighbors[neighbor1] = right;
+            left.NeighborRotation[neighbor1] = (neighbor2 - neighbor1 + 6) % 4;
+            right.Neighbors[neighbor2] = left;
+            right.NeighborRotation[neighbor2] = (neighbor1 - neighbor2 + 6) % 4;
+            left = left.Neighbors[direction1];
+            right = right.Neighbors[direction2];
+        }
+    }
+
+    private static int UpdateFacing(Tile corner, int direction)
+    {
+        Tile neighbor = corner.Neighbors[direction];
+        char[] path = Enumerable.Repeat('R', direction).Append('1').ToArray();
+        int result = FollowPath(string.Join(null, path), corner, true);
+        return (result - 1000 * neighbor.Row - 4 * neighbor.Column) % 4;
+    }
+
+    private static bool CornerIsConnected(Tile corner, int direction)
+    {
+        StringBuilder sb = new();
+        for (int i = 0; i < direction; i++)
+        {
+            sb.Append('R');
+        }
+        sb.Append("1R1R1");
+        int finalTurns = (direction + 3) % 4;
+        for (int i = 0; i < finalTurns; i++)
+        {
+            sb.Append('L');
+        }
+        return FollowPath(sb.ToString(), corner, true) == 1000 * corner.Row + 4 * corner.Column;
+    }
+
+    private static void DisconnectEdges(Section?[,] sections)
+    {
+        int width = sections.GetLength(0);
+        int height = sections.GetLength(1);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Section? current = sections[x, y];
+                if (current is null)
+                {
+                    continue;
+                }
+                if (x == 0 || sections[x - 1, y] == null)
+                {
+                    // disconnect left edge
+                    for (int i = 0; i < current.Size; i++)
+                    {
+                        current.Tiles[0, i].DisconnectNeighbor(2);
+                    }
+                }
+                if (y == 0 || sections[x, y - 1] == null)
+                {
+                    // disconnect top edge
+                    for (int i = 0; i < current.Size; i++)
+                    {
+                        current.Tiles[i, 0].DisconnectNeighbor(3);
+                    }
+                }
+                if (x == width - 1 || sections[x + 1, y] == null)
+                {
+                    // disconnect right edge
+                    for (int i = 0; i < current.Size; i++)
+                    {
+                        current.Tiles[current.Size - 1, i].DisconnectNeighbor(0);
+                    }
+                }
+                if (y == height - 1 || sections[x, y + 1] == null)
+                {
+                    // disconnect bottom edge
+                    for (int i = 0; i < current.Size; i++)
+                    {
+                        current.Tiles[i, current.Size - 1].DisconnectNeighbor(1);
+                    }
+                }
+            }
+        }
     }
 
     private class Section
@@ -240,6 +401,7 @@ internal class Day22 : Day
         public int Row { get; }
         public int Column { get; }
         public Tile[] Neighbors { get; }
+        public int[] NeighborRotation { get; }
 
         public Tile(char contents, int row, int column)
         {
@@ -247,6 +409,12 @@ internal class Day22 : Day
             Row = row;
             Column = column;
             Neighbors = [this, this, this, this];
+            NeighborRotation = [0, 0, 0, 0];
+        }
+
+        public void DisconnectNeighbor(int index)
+        {
+            Neighbors[index] = this;
         }
     }
 }
